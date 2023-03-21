931161ebbaa612cfc0aec85d53e2bd1ae85461bf
Daithi Hearn <daithi.hearn@gmail.com>
2023-03-21 23:26:04 +0100

    Adjusting throttle period

--- src/main/kotlin/org/vechain/indexer/Indexer.kt
+++ src/main/kotlin/org/vechain/indexer/Indexer.kt
@@ -7,7 +7,7 @@ enum class Status {
     SYNCING, FULLY_SYNCED, THROTTLED
 }
 const val APPROX_BLOCK_PERIOD = 9990L
-const val THROTTLE_PERIOD = 15000L
+const val THROTTLE_PERIOD = 5000L
 abstract class Indexer {
     private var status = Status.SYNCING
     var currentBlock: Long = 0
