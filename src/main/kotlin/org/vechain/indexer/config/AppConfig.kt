59cb25e0408513456afb6ea209551d8cb971ee7e
Nawfal Labrahmi <15235627+nawfal-labrahmi@users.noreply.github.com>
2023-03-23 15:02:26 +0100

    minors cleanup (#4)

--- src/main/kotlin/org/vechain/indexer/config/AppConfig.kt
+++ src/main/kotlin/org/vechain/indexer/config/AppConfig.kt
@@ -1,11 +1,9 @@
 package org.vechain.indexer.config
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.codec.ClientCodecConfigurer
-import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
 
