c921db6dd4fa0a4559423d45d2cee606cb3534f9
Daithi Hearn <daithi.hearn@gmail.com>
2023-03-24 01:56:07 +0100

    Adding profiles to allow testing of a single indexer

--- src/main/kotlin/org/vechain/indexer/ClauseIndexer.kt
+++ src/main/kotlin/org/vechain/indexer/ClauseIndexer.kt
@@ -1,12 +1,14 @@
 package org.vechain.indexer
 
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import org.vechain.indexer.model.WrappedClause
 import org.vechain.indexer.repos.ClauseRepo
 import org.vechain.indexer.service.ThorService
 
+@Profile("clause-indexer", "prod")
 @Component
-class ClauseIndexer(private val thorService: ThorService, private val clauseRepo: ClauseRepo): Indexer() {
+class ClauseIndexer(private val thorService: ThorService, private val clauseRepo: ClauseRepo) : Indexer() {
     override fun processBlock(blockNumber: Long) {
         val block = thorService.getBlock(blockNumber)
         if (block.transactions.isNotEmpty() && block.transactions.any { it.clauses.isNotEmpty() }) {
--- src/main/kotlin/org/vechain/indexer/TransactionIndexer.kt
+++ src/main/kotlin/org/vechain/indexer/TransactionIndexer.kt
@@ -1,12 +1,14 @@
 package org.vechain.indexer
 
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import org.vechain.indexer.model.WrappedTransaction
 import org.vechain.indexer.repos.TransactionRepo
 import org.vechain.indexer.service.ThorService
 
+@Profile("transaction-indexer", "prod")
 @Component
-class TransactionIndexer(private val thorService: ThorService, private val txRepo: TransactionRepo): Indexer() {
+class TransactionIndexer(private val thorService: ThorService, private val txRepo: TransactionRepo) : Indexer() {
     override fun processBlock(blockNumber: Long) {
         val block = thorService.getBlock(blockNumber)
         if (block.transactions.isNotEmpty())
