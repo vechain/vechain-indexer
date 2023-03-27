482d2423ab8e34e7c36b0f1b853210ace85974b2
Darren Kelly <darren.kelly@vechain.org>
2023-03-27 16:13:21 +0100

    add actuator healthchecks

---
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/config/ThorHealthIndicator.kt
@@ -0,0 +1,19 @@
+package org.vechain.indexer.config
+
+import org.springframework.boot.actuate.health.Health
+import org.springframework.boot.actuate.health.HealthIndicator
+import org.springframework.stereotype.Component
+import org.vechain.indexer.service.ThorService
+
+@Component
+class ThorHealthIndicator(private val thorService: ThorService) : HealthIndicator {
+    override fun health(): Health {
+        val key = "VeChainThor"
+        return try {
+            thorService.getBlock(1)
+            Health.up().withDetail(key, "Available").build()
+        } catch (e: Exception) {
+            Health.down().withDetail(key, "Unavailable").build()
+        }
+    }
+}
