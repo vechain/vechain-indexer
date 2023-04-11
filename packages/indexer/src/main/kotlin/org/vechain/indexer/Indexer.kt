bfb72557fa5a79765207fb9b8603c6aa40626492
Daithi Hearn <daithi.hearn@gmail.com>
2023-04-11 11:42:27 +0200

    Attempting to improve the backoff logic The aim is to try and stay as close to the latet block as possible

--- packages/indexer/src/main/kotlin/org/vechain/indexer/Indexer.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/Indexer.kt
@@ -7,7 +7,8 @@ enum class Status {
     SYNCING, FULLY_SYNCED
 }
 
-const val APPROX_BLOCK_PERIOD = 9990L
+const val INITIAL_BACKOFF_PERIOD = 9650L
+const val MAX_BACKOFF_PERIOD = 9950L
 
 abstract class Indexer {
 
@@ -15,6 +16,7 @@ abstract class Indexer {
 
     var status = Status.SYNCING
     var currentBlock: Long = 0
+    private var backoffPeriod = INITIAL_BACKOFF_PERIOD
 
     fun start() {
         currentBlock = getStartingBlock()
@@ -28,11 +30,12 @@ abstract class Indexer {
 
             logger.info("${name()} is processing block $currentBlock (Status: $status)")
             processBlock(currentBlock)
-
+            
+            backoffPeriod = minOf(maxOf(INITIAL_BACKOFF_PERIOD, backoffPeriod + 25), MAX_BACKOFF_PERIOD)
             currentBlock++
         } catch (e: IndexerFullySynchronizedException) {
             logger.info("${name()} is fully synchronized...")
-            Thread.sleep(1000)
+            backoffPeriod = 500
             status = Status.FULLY_SYNCED
         } catch (e: Exception) {
             logger.error("${name()}: Error while processing block $currentBlock", e)
@@ -46,7 +49,8 @@ abstract class Indexer {
 
     private fun backoffDelay() {
         if (status == Status.FULLY_SYNCED) {
-            Thread.sleep(APPROX_BLOCK_PERIOD)
+            logger.debug("Backing off for $backoffPeriod ms...")
+            Thread.sleep(backoffPeriod)
         }
     }
 
