c2b659bdf5d513008c1fd11258418059573da346
Nawfal Labrahmi <15235627+nawfal-labrahmi@users.noreply.github.com>
2023-04-13 14:20:46 +0200

    add clauses api with pagination detail (#76)

---
+++ packages/api/src/main/kotlin/org/vechain/indexer/controller/ClauseController.kt
@@ -0,0 +1,60 @@
+package org.vechain.indexer.controller
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.enums.ParameterIn
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.data.domain.Sort.Direction.ASC
+import org.springframework.data.domain.Sort.by
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.vechain.indexer.constants.CLAUSES_PATH
+import org.vechain.indexer.model.PaginatedResponse
+import org.vechain.indexer.model.PaginationDetail
+import org.vechain.indexer.model.WrappedClause
+import org.vechain.indexer.service.ClauseService
+import org.vechain.indexer.utils.PaginationUtils.toPageable
+
+@Tag(name = "Clause", description = "Query on chain tx clauses")
+@RestController
+@RequestMapping(CLAUSES_PATH)
+open class ClauseController(private val clauseService: ClauseService) {
+
+    @GetMapping
+    @Operation(summary = "Get all on chain tx clauses")
+    @Parameter(
+        `in` = ParameterIn.QUERY,
+        name = "page",
+        schema = Schema(type = "Integer"),
+        description = "The results page number",
+        required = false,
+        example = "0"
+    )
+    @Parameter(
+        `in` = ParameterIn.QUERY,
+        name = "size",
+        schema = Schema(type = "Integer"),
+        description = "The results page size",
+        required = false,
+        example = "20"
+    )
+    open fun getAllClauses(
+        @RequestParam(required = false) page: Int?,
+        @RequestParam(required = false) size: Int?,
+    ): PaginatedResponse<List<WrappedClause>> {
+        val resultsPage = clauseService.findAll(toPageable(page, size, by(ASC, "blockNumber", "txId", "id")))
+
+        return PaginatedResponse(
+            data = resultsPage.content,
+            pagination = PaginationDetail(
+                totalPages = resultsPage.totalPages,
+                totalElements = resultsPage.totalElements
+            )
+        )
+
+    }
+
+}
