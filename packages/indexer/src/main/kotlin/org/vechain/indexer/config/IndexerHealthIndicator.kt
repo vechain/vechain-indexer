0f3121c2d1116e3569941b413b82c5f8dbdfe5e2
Darren Kelly <107671032+darrenvechain@users.noreply.github.com>
2023-04-27 13:21:34 +0100

    add indexer healthcheck (#99)

---
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/config/IndexerHealthIndicator.kt
@@ -0,0 +1,48 @@
+package org.vechain.indexer.config
+
+import org.springframework.boot.actuate.health.Health
+import org.springframework.boot.actuate.health.HealthIndicator
+import org.springframework.boot.actuate.health.Status
+import org.springframework.stereotype.Component
+import org.vechain.indexer.Indexer
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+
+@Component
+class IndexerHealthIndicator(
+    private val indexers: List<Indexer>
+) : HealthIndicator {
+
+    data class IndexerHealth(val indexerName: String, val status: Status)
+
+    companion object {
+        const val PROCESS_TIMEOUT = 60L
+    }
+
+    override fun health(): Health {
+        val key = "IndexersHealth"
+
+        val indexerHealths = indexers.map { indexer ->
+            IndexerHealth(indexer.name, getIndexerHealth(indexer))
+        }
+
+        val badIndexers = indexerHealths.filter { it.status == Status.DOWN }
+
+        return if (badIndexers.isNotEmpty()) {
+            Health.down().withDetail(key, badIndexers).build()
+        } else {
+            Health.up().withDetail(key, indexerHealths).build()
+        }
+    }
+
+    private fun getIndexerHealth(indexer: Indexer): Status {
+        val timeNow = LocalDateTime.now(ZoneOffset.UTC)
+
+        val timeLastProcessed = indexer.timeLastProcessed
+        return if (timeNow.minusSeconds(PROCESS_TIMEOUT) > timeLastProcessed) {
+            Status.DOWN
+        } else {
+            Status.UP
+        }
+    }
+}
