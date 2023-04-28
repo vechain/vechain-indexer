f179363446c6ea84d44ea3e19675b78142682440
Daithi Hearn <daithi.hearn@gmail.com>
2023-04-28 14:27:26 +0200

    Adding more load tests and improving existing tests

--- load-testing/src/transfer-events-test.ts
+++ load-testing/src/transfer-events-test.ts
@@ -1,6 +1,6 @@
 import { check } from "k6";
 import http from "k6/http";
-import accounts from "./data/nft-accounts.json";
+import accounts from "./data/transfer-event-accounts.json";
 import { randomElement } from "./utils/array-utils";
 import env from "./env";
 import { DEFAULT_OPTIONS } from "./constants";
@@ -8,16 +8,12 @@ import { DEFAULT_OPTIONS } from "./constants";
 export const options = DEFAULT_OPTIONS
 
 /**
- *  Make a GET request to the NFT endpoint using a random address
+ *  Make a GET request to the transactions endpoint using a random address
  */
 export default () => {
   const account = randomElement(accounts);
 
-  const res = http.get(`${env.BASE_URL}/api/v1/nfts?address=${account}`);
-
-  check(res, {
-    "status is 200": () => res.status === 200,
-  });
+  const res = http.get(`${env.BASE_URL}/api/v1/transfers?address=${account}`);
 
   check(res, {
     "status is 200": () => res.status === 200,
@@ -30,4 +26,5 @@ export default () => {
       }
     },
   });
+
 };
