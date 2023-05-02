9cba704f7d8d44a450d10801f353bcbba3fdcac3
Daithi Hearn <daithi.hearn@gmail.com>
2023-05-02 16:01:28 +0200

    Adding http and https servers for swagger

--- packages/api/src/main/kotlin/org/vechain/indexer/VeWorldIndexerApiApplication.kt
+++ packages/api/src/main/kotlin/org/vechain/indexer/VeWorldIndexerApiApplication.kt
@@ -2,6 +2,7 @@ package org.vechain.indexer
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -11,7 +12,11 @@ import org.springframework.boot.runApplication
         "VeWorld Indexer API",
         version = "0.0.1",
         description = "Blockchain data indexed for fast querying"
-    )
+    ),
+    servers = [
+        Server(url = "https://", description = "HTTPS server"),
+        Server(url = "http://", description = "HTTP server")
+    ]
 )
 @SpringBootApplication(scanBasePackages = ["org.vechain.indexer"])
 open class VeWorldIndexerApiApplication
