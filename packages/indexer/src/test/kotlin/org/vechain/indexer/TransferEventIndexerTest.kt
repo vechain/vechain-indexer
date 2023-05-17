adfb60467425a2649a60fd9514c025da972e70b4
Darren Kelly <darren.kelly@vechain.org>
2023-05-17 17:22:18 +0100

    update unit test

--- packages/indexer/src/test/kotlin/org/vechain/indexer/TransferEventIndexerTest.kt
+++ packages/indexer/src/test/kotlin/org/vechain/indexer/TransferEventIndexerTest.kt
@@ -86,7 +86,7 @@ class TransferEventIndexerTest {
             that(transfers).hasSize(10)
         }
         val transferEvent =
-            transfers.first { it.id == DigestUtils.sha1Hex("0xe896f18857b416ea5553be739848911ee75593012f4853e775f39bef10eeae2e-TOPIC-9-0") }
+            transfers.first { it.id == DigestUtils.sha1Hex("0xe896f18857b416ea5553be739848911ee75593012f4853e775f39bef10eeae2e-TOPIC-9-0-0") }
         expect {
             that(transferEvent.blockId).isEqualTo("0x00000008de120e47e15edb8d9a23823b198590623c3c9f938c5f623f13e7402e")
             that(transferEvent.blockNumber).isEqualTo(blockNumber)
@@ -94,7 +94,7 @@ class TransferEventIndexerTest {
             that(transferEvent.txId).isEqualTo("0xe896f18857b416ea5553be739848911ee75593012f4853e775f39bef10eeae2e")
             that(transferEvent.from).isEqualTo("0x0000000000000000000000000000000000000000")
             that(transferEvent.to).isEqualTo("0xd7f75a0a1287ab2916848909c8531a0ea9412800")
-            that(transferEvent.value).isEqualTo(BigInteger.ZERO)
+            that(transferEvent.value).isEqualTo(BigInteger.ONE)
             that(transferEvent.tokenAddress).isEqualTo("0x1f734d58eb6a349f038c28f112478bf90981c87e")
             that(transferEvent.topics).hasSize(4).and {
                 that(transferEvent.topics[0]).isEqualTo("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef")
