160200e1334c0bd327310d3fb3302cd618a8394e
Darren Kelly <darren.kelly@vechain.org>
2023-05-18 09:35:30 +0100

    update transfer events tests

--- packages/e2e/build.gradle.kts
+++ packages/e2e/build.gradle.kts
@@ -2,20 +2,20 @@ dependencies {
     implementation(project(":packages:common"))
 }
 
-task<Exec>("preE2e") {
-    environment("ENV_FILE_NAME", ".env.example")
-    workingDir(rootDir)
-    commandLine("make", "clean", "start")
-}
-
-task<Exec>("postE2e") {
-    environment("ENV_FILE_NAME", ".env.example")
-    workingDir(rootDir)
-    //Not cleaning data in case we need to spin up the containers again and debug
-    commandLine("make", "down")
-}
+//task<Exec>("preE2e") {
+//    environment("ENV_FILE_NAME", ".env.example")
+//    workingDir(rootDir)
+//    commandLine("make", "clean", "start")
+//}
+//
+//task<Exec>("postE2e") {
+//    environment("ENV_FILE_NAME", ".env.example")
+//    workingDir(rootDir)
+//    //Not cleaning data in case we need to spin up the containers again and debug
+//    commandLine("make", "down")
+//}
 
 tasks.test {
-    dependsOn("preE2e")
-    finalizedBy("postE2e")
+//    dependsOn("preE2e")
+//    finalizedBy("postE2e")
 }
