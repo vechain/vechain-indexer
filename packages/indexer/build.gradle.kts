54aeb88070b6ffe1083df16c203619fce88d66f9
Daithi Hearn <37153003+daithihearn@users.noreply.github.com>
2023-05-25 10:12:37 +0200

    Adding specific versions for all dependencies (#149)

--- packages/indexer/build.gradle.kts
+++ packages/indexer/build.gradle.kts
@@ -2,7 +2,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 dependencies {
     implementation(project(":packages:common"))
-    implementation("org.web3j:utils:4.9.7")
+    implementation("org.web3j:utils:4.9.8")
     implementation("com.github.vechain:thor-devkit.java:v1.0.0")
 }
 
