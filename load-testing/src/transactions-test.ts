3f4d89a06c0b01c24bba529e474233c712f741c5
Daithi Hearn <daithi.hearn@gmail.com>
2023-06-05 14:14:33 +0200

    Fixing load tests

--- load-testing/src/transactions-test.ts
+++ load-testing/src/transactions-test.ts
@@ -1,6 +1,6 @@
 import { check } from "k6";
 import http from "k6/http";
-import txIds from "./data/transaction-ids.json";
+import accounts from "./data/clause-accounts.json";
 import { randomElement } from "./utils/array-utils";
 import env from "./env";
 import { DEFAULT_OPTIONS } from "./constants";
@@ -11,16 +11,16 @@ export const options = DEFAULT_OPTIONS
  *  Make a GET request to the transactions endpoint using a random address
  */
 export default () => {
-  const id = randomElement(txIds);
+  const acct = randomElement(accounts);
 
-  const res = http.get(`${env.BASE_URL}/api/v1/transactions?id=${id}`);
+  const res = http.get(`${env.BASE_URL}/api/v1/transactions?origin=${acct}`);
 
   check(res, {
     "status is 200": () => res.status === 200,
     "id has the expected value": () => {
       if (typeof res.body === "string") {
         const body = JSON.parse(res.body);
-        return body.id === id;
+        return body.length > 0;
       } else {
         return false;
       }
