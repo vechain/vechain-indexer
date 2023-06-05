da4c63e3d373863268cd262c9961c192a8116498
nawfal-labrahmi <nawfal.labrahmi@gmail.com>
2023-06-06 00:37:14 +0200

    refactor custom query

--- packages/api/src/main/kotlin/org/vechain/indexer/service/NFTService.kt
+++ packages/api/src/main/kotlin/org/vechain/indexer/service/NFTService.kt
@@ -7,8 +7,6 @@ import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.aggregation.Aggregation
-import org.springframework.data.mongodb.core.aggregation.CountOperation
-import org.springframework.data.mongodb.core.aggregation.MatchOperation
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.stereotype.Service
@@ -38,16 +36,18 @@ open class NFTService(private val nftRepo: NFTRepo, private val mongoTemplate: M
     }
 
     open fun findContractsByNFTOwner(owner: String, pageable: Pageable): Page<String> {
-        val query = Query(Criteria.where("owner").`is`(HexUtil.normalise(owner)))
-            .with(pageable)
+        val ownerCriteria = Criteria.where("owner").`is`(HexUtil.normalise(owner))
+        val query = Query(ownerCriteria).with(pageable)
 
         // count distinct contracts
-        val matchStage: MatchOperation = Aggregation.match(Criteria.where("owner").`is`(HexUtil.normalise(owner)))
-        val groupOperation = Aggregation.group("contractAddress")
-        val countOperation: CountOperation = Aggregation.count().`as`("total")
-        val aggregation = Aggregation.newAggregation(matchStage, groupOperation, countOperation)
-        val result: Document? =
-            mongoTemplate.aggregate(aggregation, IndexedNFT::class.java, Document::class.java).uniqueMappedResult
+        val aggregation = Aggregation.newAggregation(
+            Aggregation.match(ownerCriteria),
+            Aggregation.group("contractAddress"),
+            Aggregation.count().`as`("total")
+        )
+        val result = mongoTemplate
+            .aggregate(aggregation, IndexedNFT::class.java, Document::class.java)
+            .uniqueMappedResult
         val count = if (result == null) 0 else result.getInteger("total")
 
         // find distinct contracts
