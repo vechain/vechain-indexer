285f1c1937adab63bceb57df70276767ce9be0cb
Darren Kelly <darren.kelly@vechain.org>
2023-06-07 13:00:52 +0100

    testing new tags for images

--- packages/common/src/main/kotlin/org/vechain/indexer/repos/ContractRepoImpl.kt
+++ packages/common/src/main/kotlin/org/vechain/indexer/repos/ContractRepoImpl.kt
@@ -11,6 +11,10 @@ import org.vechain.indexer.model.rest.ContractType
 @Repository
 open class ContractRepoImpl(private val mongoTemplate: MongoTemplate) {
 
+    companion object {
+        val CREATOR = IndexedContract::creator.name
+    }
+
     open fun findById(address: String): IndexedContract? {
         return mongoTemplate.findById(address, IndexedContract::class.java)
     }
@@ -22,7 +26,7 @@ open class ContractRepoImpl(private val mongoTemplate: MongoTemplate) {
     ): List<IndexedContract> {
         val query = Query().with(pageable)
 
-        if (creator != null) query.addCriteria(Criteria.where("creator").`is`(creator))
+        if (creator != null) query.addCriteria(Criteria.where(CREATOR).`is`(creator))
         if (contractType != null) addTypeCriteria(contractType, query)
 
         return mongoTemplate.find(query, IndexedContract::class.java)
@@ -30,12 +34,12 @@ open class ContractRepoImpl(private val mongoTemplate: MongoTemplate) {
 
     private fun addTypeCriteria(contractType: ContractType, query: Query) {
         val criteriaKey = when (contractType) {
-            ContractType.VIP180 -> "isVip180"
-            ContractType.VIP181 -> "isVip181"
-            ContractType.VIP210 -> "isVip210"
-            ContractType.ERC20 -> "isErc20"
-            ContractType.ERC721 -> "isErc721"
-            ContractType.ERC1155 -> "isErc1155"
+            ContractType.VIP180 -> IndexedContract::isVip180.name
+            ContractType.VIP181 -> IndexedContract::isVip181.name
+            ContractType.VIP210 -> IndexedContract::isVip210.name
+            ContractType.ERC20 -> IndexedContract::isErc20.name
+            ContractType.ERC721 -> IndexedContract::isErc721.name
+            ContractType.ERC1155 -> IndexedContract::isErc1155.name
         }
         query.addCriteria(Criteria.where(criteriaKey).`is`(true))
     }
