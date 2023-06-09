cfa9451e9bcd8dffb217543a9e2fca8e2d8c62a5
Daithi Hearn <daithi.hearn@gmail.com>
2023-06-09 06:32:30 +0200

    Refactoring if/else statement to a when statement

--- packages/indexer-core/build.gradle.kts
+++ packages/indexer-core/build.gradle.kts
@@ -1,7 +1,6 @@
 dependencies {
     implementation(project(":packages:thor-model"))
 
-
     // Other dependencies...
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
 
