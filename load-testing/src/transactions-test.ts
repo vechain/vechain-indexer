cdd88981b092d6bcba8295f290c64c5257c4b7a7
nawfal-labrahmi <nawfal.labrahmi@gmail.com>
2023-06-15 16:41:26 +0200

    update load test

--- load-testing/src/transactions-test.ts
+++ load-testing/src/transactions-test.ts
@@ -1,9 +1,9 @@
-import { check } from "k6";
+import {check} from "k6";
 import http from "k6/http";
 import accounts from "./data/clause-accounts.json";
-import { randomElement } from "./utils/array-utils";
+import {randomElement} from "./utils/array-utils";
 import env from "./env";
-import { DEFAULT_OPTIONS } from "./constants";
+import {DEFAULT_OPTIONS} from "./constants";
 
 export const options = DEFAULT_OPTIONS
 
@@ -11,19 +11,19 @@ export const options = DEFAULT_OPTIONS
  *  Make a GET request to the transactions endpoint using a random address
  */
 export default () => {
-  const acct = randomElement(accounts);
+    const acct = randomElement(accounts);
 
-  const res = http.get(`${env.BASE_URL}/api/v1/transactions?origin=${acct}`);
+    const res = http.get(`${env.BASE_URL}/api/v1/transactions?origin=${acct}`);
 
-  check(res, {
-    "status is 200": () => res.status === 200,
-    "id has the expected value": () => {
-      if (typeof res.body === "string") {
-        const body = JSON.parse(res.body);
-        return body.length > 0;
-      } else {
-        return false;
-      }
-    },
-  });
+    check(res, {
+        "status is 200": () => res.status === 200,
+        "has results": () => {
+            if (typeof res.body === "string") {
+                const body = JSON.parse(res.body);
+                return body.data.length > 0;
+            } else {
+                return false;
+            }
+        },
+    });
 };
