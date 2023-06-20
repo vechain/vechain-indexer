95828e45afcb8a7d55452364f2c7f8c24650e65e
Daithi Hearn <daithi.hearn@gmail.com>
2023-06-20 12:20:36 +0200

    Fixing NFT load tests Adding separate tests for NFT contracts

--- load-testing/src/nft-contracts-test.ts
+++ load-testing/src/nft-contracts-test.ts
@@ -8,18 +8,18 @@ import {DEFAULT_OPTIONS} from "./constants";
 export const options = DEFAULT_OPTIONS
 
 /**
- *  Make a GET request to the NFT endpoint using a random address
+ *  Make a GET request to the NFT contracts endpoint using a random address
  */
 export default () => {
     const account = randomElement(accounts);
 
-    const nfts = http.get(`${env.BASE_URL}/api/v1/nfts?address=${account}`);
+    const nftContracts = http.get(`${env.BASE_URL}/api/v1/nfts/contracts?owner=${account}`);
 
-    check(nfts, {
-        "status is 200": () => nfts.status === 200,
+    check(nftContracts, {
+        "status is 200": () => nftContracts.status === 200,
         "has results": () => {
-            if (typeof nfts.body === "string") {
-                const body = JSON.parse(nfts.body);
+            if (typeof nftContracts.body === "string") {
+                const body = JSON.parse(nftContracts.body);
                 return body.data.length > 0;
             } else {
                 return false;
