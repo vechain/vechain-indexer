1813062073c6a00670e6628059dd428e016d883b
Darren Kelly <107671032+darrenvechain@users.noreply.github.com>
2023-06-21 22:53:54 +0100

    logging app health (#239)

---
+++ packages/common/src/main/kotlin/org/vechain/indexer/config/ApplicationHealth.kt
@@ -0,0 +1,26 @@
+package org.vechain.indexer.config
+
+import org.slf4j.LoggerFactory
+import org.springframework.boot.actuate.health.HealthEndpoint
+import org.springframework.boot.actuate.health.Status
+import org.springframework.boot.actuate.health.SystemHealth
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
+open class ApplicationHealth(private val healthEndpoint: HealthEndpoint) {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    @Scheduled(fixedDelay = 60 * 1000)
+    fun logApplicationHealth() {
+        val health = healthEndpoint.health() as SystemHealth
+
+        if (health.status != Status.UP) {
+            logger.error("Application is UNHEALTHY: {}", health.components.filter { it.value.status != Status.UP })
+        } else {
+            logger.info("Application is healthy")
+        }
+    }
+
+}
