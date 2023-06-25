d48d57a7bb5960eadb66ccd9c34bd6b66a5bac60
Darren Kelly <darren.kelly@vechain.org>
2023-06-25 20:31:41 +0100

    defining the block number in archives

--- packages/common/src/main/kotlin/org/vechain/indexer/model/Archive.kt
+++ packages/common/src/main/kotlin/org/vechain/indexer/model/Archive.kt
@@ -3,7 +3,7 @@ package org.vechain.indexer.model
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "archives")
-data class Archive(
+data class Archive<T : IndexedDocument>(
     val id: String,
-    val data: Any,
+    val data: T
 )
