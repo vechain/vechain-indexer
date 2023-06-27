87637bc87207bea87ca9283a027f19b6ff443a37
nawfal-labrahmi <nawfal.labrahmi@gmail.com>
2023-06-27 10:24:37 +0200

    Add load tests for different endpoints

---
+++ load-testing/src/nfts-by-owner-contract-address-test.ts
@@ -0,0 +1,31 @@
+import {check} from "k6";
+import http from "k6/http";
+import accounts from "./data/nft-accounts.json";
+import contracts from "./data/contract-addresses.json";
+import {randomElement} from "./utils/array-utils";
+import env from "./env";
+import {DEFAULT_OPTIONS} from "./constants";
+
+export const options = DEFAULT_OPTIONS
+
+/**
+ *  Make a GET request to the NFT endpoint using a random address
+ */
+export default () => {
+    const account = randomElement(accounts);
+    const contractAddress = randomElement(contracts);
+
+    const nfts = http.get(`${env.BASE_URL}/api/v1/nfts?address=${account}&contractAddress=${contractAddress}`);
+
+    check(nfts, {
+        "status is 200": () => nfts.status === 200,
+        "has results": () => {
+            if (typeof nfts.body === "string") {
+                const body = JSON.parse(nfts.body);
+                return body.data.length > 0;
+            } else {
+                return false;
+            }
+        },
+    });
+};
