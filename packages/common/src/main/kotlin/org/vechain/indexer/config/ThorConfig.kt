14fed2d719656f8a24dd5a9ba380d3deecc36c69
nawfal-labrahmi <nawfal.labrahmi@gmail.com>
2023-06-27 12:35:31 +0200

    align spring profiles

--- packages/common/src/main/kotlin/org/vechain/indexer/config/ThorConfig.kt
+++ packages/common/src/main/kotlin/org/vechain/indexer/config/ThorConfig.kt
@@ -3,10 +3,12 @@ package org.vechain.indexer.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.http.codec.ClientCodecConfigurer
 import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
 
+@Profile("blocks-proxy")
 @Configuration
 open class ThorConfig(@Value("\${thor.url}") private val thorUrl: String) {
 
--- packages/common/src/main/kotlin/org/vechain/indexer/config/ThorHealthIndicator.kt
+++ packages/common/src/main/kotlin/org/vechain/indexer/config/ThorHealthIndicator.kt
@@ -2,10 +2,12 @@ package org.vechain.indexer.config
 
 import org.springframework.boot.actuate.health.Health
 import org.springframework.boot.actuate.health.HealthIndicator
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.vechain.thor.model.Block
 
+@Profile("blocks-proxy")
 @Component
 class ThorHealthIndicator(private val thorRest: WebClient) : HealthIndicator {
     override fun health(): Health {
