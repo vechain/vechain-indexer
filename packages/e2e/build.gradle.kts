e61e324832e3df0835bb3ee16aead9e0a1502034
Darren Kelly <darren.kelly@vechain.org>
2023-06-29 10:12:43 +0100

    update pre commit script

--- packages/e2e/build.gradle.kts
+++ packages/e2e/build.gradle.kts
@@ -1,6 +1,4 @@
-dependencies {
-    implementation(project(":packages:common"))
-}
+dependencies { implementation(project(":packages:common")) }
 
 task<Exec>("preE2e") {
     environment("INDEXER_ENV_FILE_NAME", "packages/indexer/.env.example")
@@ -13,11 +11,11 @@ task<Exec>("postE2e") {
     environment("INDEXER_ENV_FILE_NAME", "packages/indexer/.env.example")
     environment("API_ENV_FILE_NAME", "packages/api/.env.example")
     workingDir(rootDir)
-    //Not cleaning data in case we need to spin up the containers again and debug
+    // Not cleaning data in case we need to spin up the containers again and debug
     commandLine("make", "down", "db-keyfile-remove")
 }
 
 tasks.test {
-    dependsOn("preE2e")
-    finalizedBy("postE2e")
+    //    dependsOn("preE2e")
+    //    finalizedBy("postE2e")
 }
