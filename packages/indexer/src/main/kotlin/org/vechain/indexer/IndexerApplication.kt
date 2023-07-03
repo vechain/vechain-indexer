ae47a6dfac1916230a04bde3bf66de621baa8793
nawfal-labrahmi <nawfal.labrahmi@gmail.com>
2023-07-03 18:02:28 +0200

    Setup mongock

--- packages/indexer/src/main/kotlin/org/vechain/indexer/IndexerApplication.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/IndexerApplication.kt
@@ -1,9 +1,11 @@
 package org.vechain.indexer
 
+import io.mongock.runner.springboot.EnableMongock
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableMongock
 @SpringBootApplication(scanBasePackages = ["org.vechain.indexer"])
 @EnableScheduling
 open class IndexerApplication
