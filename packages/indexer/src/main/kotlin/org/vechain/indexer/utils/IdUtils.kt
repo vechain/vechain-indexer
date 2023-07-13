74897b38bcd1fb6c6b8fbb6a257f3bcaba6ef098
nawfal-labrahmi <nawfal.labrahmi@gmail.com>
2023-07-13 15:36:20 +0200

    Fix NFTs update

--- packages/indexer/src/main/kotlin/org/vechain/indexer/utils/IdUtils.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/utils/IdUtils.kt
@@ -1,11 +1,19 @@
 package org.vechain.indexer.utils
 
 import org.apache.commons.codec.digest.DigestUtils
+import org.vechain.indexer.model.IndexedTransferEvent
 import org.vechain.indexer.model.VersionedDocument
+import org.web3j.utils.Numeric
 
 object IdUtils {
-    fun buildHashedId(plainId: String) = DigestUtils.sha1Hex(plainId)
 
     fun buildArchiveId(document: VersionedDocument, version: Int = document.version): String =
         buildHashedId("${document::class.simpleName}-${document.getDocumentId()}-${version}")
+
+    fun buildNftId(transferEvent: IndexedTransferEvent): String =
+        buildHashedId(
+            "${transferEvent.tokenAddress}-${Numeric.parsePaddedNumberHex(transferEvent.topics[3])}"
+        )
 }
+
+private fun buildHashedId(plainId: String): String = DigestUtils.sha1Hex(plainId)
