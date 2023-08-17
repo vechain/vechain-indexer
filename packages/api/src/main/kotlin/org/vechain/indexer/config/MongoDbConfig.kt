8310b22f096d41fab19e6f491d49b3d9c62592b8
Darren Kelly <darren.kelly@vechain.org>
2023-08-17 13:15:00 +0100

    update URI

--- packages/api/src/main/kotlin/org/vechain/indexer/config/MongoDbConfig.kt
+++ packages/api/src/main/kotlin/org/vechain/indexer/config/MongoDbConfig.kt
@@ -1,50 +1,8 @@
 package org.vechain.indexer.config
 
-import com.mongodb.ConnectionString
-import com.mongodb.MongoClientSettings
-import com.mongodb.MongoCredential
-import com.mongodb.client.MongoClient
-import com.mongodb.client.MongoClients
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
-import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @Configuration
 @EnableMongoRepositories(basePackages = ["org.vechain.indexer.repository"])
-open class MongoDbConfig(
-    @Value("\${spring.data.mongodb.uri}") private val mongoUri: String,
-    @Value("\${spring.data.mongodb.username}") private val mongoUser: String,
-    @Value("\${spring.data.mongodb.password}") private val mongoPassword: String,
-    @Value("\${spring.data.mongodb.authentication-database}") private val authDb: String
-) {
-
-    private val connectionString: ConnectionString = ConnectionString(mongoUri)
-
-    @Bean
-    @Primary
-    open fun mongoClient(): MongoClient {
-
-        val settings =
-            MongoClientSettings.builder()
-                .applyConnectionString(connectionString)
-                .credential(
-                    MongoCredential.createCredential(mongoUser, authDb, mongoPassword.toCharArray())
-                )
-                .build()
-        return MongoClients.create(settings)
-    }
-
-    @Bean
-    @Primary
-    open fun mongoTemplate(mongoClient: MongoClient): MongoTemplate {
-
-        val database =
-            connectionString.database
-                ?: throw IllegalArgumentException("MongoDB URI must contain database name")
-
-        return MongoTemplate(mongoClient, database)
-    }
-}
+open class MongoDbConfig
--- packages/indexer/src/main/kotlin/org/vechain/indexer/config/MongoDbConfig.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/config/MongoDbConfig.kt
@@ -1,56 +1,17 @@
 package org.vechain.indexer.config
 
-import com.mongodb.ConnectionString
-import com.mongodb.MongoClientSettings
-import com.mongodb.MongoCredential
-import com.mongodb.client.MongoClient
-import com.mongodb.client.MongoClients
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import org.springframework.data.mongodb.MongoTransactionManager
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @Configuration
 @EnableMongoRepositories(basePackages = ["org.vechain.indexer"])
-open class MongoDbConfig(
-    @Value("\${spring.data.mongodb.uri}") private val mongoUri: String,
-    @Value("\${spring.data.mongodb.username}") private val mongoUser: String,
-    @Value("\${spring.data.mongodb.password}") private val mongoPassword: String,
-    @Value("\${spring.data.mongodb.authentication-database}") private val authDb: String
-) {
-
-    private val connectionString: ConnectionString = ConnectionString(mongoUri)
-
-    @Bean
-    @Primary
-    open fun mongoClient(): MongoClient {
-
-        val settings =
-            MongoClientSettings.builder()
-                .applyConnectionString(connectionString)
-                .credential(
-                    MongoCredential.createCredential(mongoUser, authDb, mongoPassword.toCharArray())
-                )
-                .build()
-        return MongoClients.create(settings)
-    }
-
-    @Bean
-    @Primary
-    open fun mongoTemplate(mongoClient: MongoClient): MongoTemplate {
-
-        val database =
-            connectionString.database
-                ?: throw IllegalArgumentException("MongoDB URI must contain database name")
-
-        return MongoTemplate(mongoClient, database)
-    }
+open class MongoDbConfig(private val mongoTemplate: MongoTemplate) {
 
     @Bean("mongoTransactionManager")
-    open fun mongoTransactionManager(mongoTemplate: MongoTemplate): MongoTransactionManager {
+    open fun mongoTransactionManager(): MongoTransactionManager {
         return MongoTransactionManager(mongoTemplate.mongoDatabaseFactory)
     }
 }
