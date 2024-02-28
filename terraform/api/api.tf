688428c58429eb1ad6ab4654ce57e92acd70222f
Dougal Rea <dougal.rea@gmail.com>
2024-02-28 10:54:18 +0000

    Remove networking modules from api.tf and update references to reflect imported modules

--- terraform/api/api.tf
+++ terraform/api/api.tf
@@ -6,100 +6,30 @@ variable "app_name" {
   default = ""
 }
 
-data "aws_vpc" "ct_vpc_id" {
-  filter {
-    name   = "tag:Name"
-    values = ["aws-controltower-VPC", "${local.env.environment}-${var.project}-vpc"]
-  }
-}
+# data "aws_vpc" "ct_vpc_id" {
+#   filter {
+#     name   = "tag:Name"
+#     values = ["aws-controltower-VPC", "${local.env.environment}-${var.project}-vpc"]
+#   }
+# }
 
-data "aws_subnets" "ct_pub_subnets" {
-  filter {
-    name   = "tag:Name"
-    values = ["aws-controltower-PublicSubnet*"]
-  }
-}
+# data "aws_subnets" "ct_pub_subnets" {
+#   filter {
+#     name   = "tag:Name"
+#     values = ["aws-controltower-PublicSubnet*"]
+#   }
+# }
 
-data "aws_subnets" "ct_priv_subnets" {
-  filter {
-    name   = "tag:Name"
-    values = ["aws-controltower-PrivateSubnet*"]
-  }
-}
-
-module "vpc" {
-  count   = local.env.environment == "prod" ? 0 : 1
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "v4.0.1"
-  cidr    = local.env.cidr
-  name    = "${local.env.environment}-${var.project}-vpc"
-  azs     = local.env.azs
-  # Compact subnetting up to 4 AZs, by up to 4 subnets by x/24 cidr blocks neatly fits in /20 172.31.0.0/16 fits 16 of them
-  # AZ's are offset by 4 with subnets if each AZ sequential neighbours
-  public_subnets         = local.env.public_subnets
-  private_subnets        = local.env.private_subnets
-  database_subnets       = local.env.database_subnets
-  enable_nat_gateway     = true
-  single_nat_gateway     = false
-  one_nat_gateway_per_az = true
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = {
-    Environment = local.env.environment
-    Terraform   = "true"
-    Project     = var.project
-  }
-}
-
-# Modules
-module "vpclogs_cloudwatch" {
-  source                                          = "git::git@github.com:/vechainfoundation/terraform_infrastructure_modules.git//logs"
-  environment                                     = local.env.environment
-  create_flow_log_cloudwatch_log_group            = "true"
-  create_flow_log_cloudwatch_iam_role             = "true"
-  flow_log_log_format                             = null
-  flow_log_traffic_type                           = "ALL"
-  vpc_id                                          = local.env.environment == "prod" ? data.aws_vpc.ct_vpc_id.id : module.vpc[0].vpc_id
-  flow_log_max_aggregation_interval               = 600
-  flow_log_destination_type                       = "cloud-watch-logs"
-  flow_log_file_format                            = "plain-text"
-  flow_log_hive_compatible_partitions             = "false"
-  flow_log_per_hour_partition                     = "false"
-  flow_log_cloudwatch_log_group_retention_in_days = 30
-  flow_log_cloudwatch_log_group_kms_key_id        = null
-  vpc_flow_log_permissions_boundary               = null
-  enable_flow_log_cloudwatch                      = "true"
-}
-
-
-module "vpclogs_s3" {
-  source                              = "git::git@github.com:/vechainfoundation/terraform_infrastructure_modules.git//logs"
-  environment                         = local.env.environment
-  app_name                            = var.project
-  flow_log_log_format                 = null
-  flow_log_traffic_type               = "ALL"
-  vpc_id                              = local.env.environment == "prod" ? data.aws_vpc.ct_vpc_id.id : module.vpc[0].vpc_id
-  flow_log_max_aggregation_interval   = 600
-  flow_log_destination_type           = "s3"
-  flow_log_file_format                = "plain-text"
-  flow_log_hive_compatible_partitions = "false"
-  flow_log_per_hour_partition         = "false"
-  vpc_flow_log_permissions_boundary   = null
-  enable_flow_log_s3                  = "false"
-  create_s3_bucket                    = "false"
-}
-
-# VPC
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = local.env.environment == "prod" ? data.aws_vpc.ct_vpc_id.id : module.vpc[0].vpc_id
-}
+# data "aws_subnets" "ct_priv_subnets" {
+#   filter {
+#     name   = "tag:Name"
+#     values = ["aws-controltower-PrivateSubnet*"]
+#   }
+# }
 
 resource "aws_service_discovery_private_dns_namespace" "ns" {
   name = "${local.env.environment}.${var.project}"
-  vpc  = local.env.environment == "prod" ? data.aws_vpc.ct_vpc_id.id : module.vpc[0].vpc_id
+  vpc  = data.terraform_remote_state.vpc.outputs.vpc_id
 }
 
 ################################################################################
@@ -109,9 +39,9 @@ resource "aws_service_discovery_private_dns_namespace" "ns" {
 module "ecs-lb-service" {
   for_each                   = local.env.enabled_nets
   source                     = "git::git@github.com:/vechainfoundation/devops.git//ecs"
-  vpc_id                     = local.env.environment == "prod" ? data.aws_vpc.ct_vpc_id.id : module.vpc[0].vpc_id
-  public_subnets             = local.env.environment == "prod" ? data.aws_subnets.ct_pub_subnets.ids : module.vpc[0].public_subnets
-  private_subnets            = local.env.environment == "prod" ? data.aws_subnets.ct_priv_subnets.ids : module.vpc[0].private_subnets
+  vpc_id                     = data.terraform_remote_state.vpc.outputs.vpc_id
+  public_subnets             = data.terraform_remote_state.vpc.outputs.public_subnets
+  private_subnets            = data.terraform_remote_state.vpc.outputs.private_subnets
   env                        = local.env.environment
   common_ecr_repo            = true
   common_ecr_repo_url        = each.value.api.ecr_common_repo
@@ -190,9 +120,9 @@ module "ecs-lb-service" {
 module "ecs-service" {
   for_each            = local.env.enabled_nets
   source              = "git::git@github.com:/vechainfoundation/devops.git//ecs"
-  vpc_id              = local.env.environment == "prod" ? data.aws_vpc.ct_vpc_id.id : module.vpc[0].vpc_id
-  public_subnets      = local.env.environment == "prod" ? data.aws_subnets.ct_pub_subnets.ids : module.vpc[0].public_subnets
-  private_subnets     = local.env.environment == "prod" ? data.aws_subnets.ct_priv_subnets.ids : module.vpc[0].private_subnets
+  vpc_id              = data.terraform_remote_state.vpc.outputs.vpc_id
+  public_subnets      = data.terraform_remote_state.vpc.outputs.public_subnets
+  private_subnets     = data.terraform_remote_state.vpc.outputs.private_subnets
   env                 = local.env.environment
   common_ecr_repo     = true
   common_ecr_repo_url = each.value.indexer.ecr_common_repo
@@ -322,7 +252,7 @@ output "security_group_ecs_service_id" {
 data "aws_security_groups" "ecs_sg_list" {
   filter {
     name   = "vpc-id"
-    values = [local.env.environment == "prod" ? data.aws_vpc.ct_vpc_id.id : module.vpc[0].vpc_id]
+    values = [data.terraform_remote_state.vpc.outputs.vpc_id]
   }
   filter {
     name   = "group-name"
@@ -336,9 +266,9 @@ module "vpc-endpoints" {
   vpcendpoints_interfaces = [
     {
       id                  = "ec2"
-      vpc_id              = local.env.environment == "prod" ? data.aws_vpc.ct_vpc_id.id : module.vpc[0].vpc_id
+      vpc_id              = data.terraform_remote_state.vpc.outputs.vpc_id
       security_group_ids  = data.aws_security_groups.ecs_sg_list.ids
-      subnet_ids          = local.env.environment == "prod" ? data.aws_subnets.ct_priv_subnets.ids : module.vpc[0].private_subnets
+      subnet_ids          = data.terraform_remote_state.vpc.outputs.private_subnets
       private_dns_enabled = true
       allowed_cidr_blocks = [local.env.cidr]
       inbound_ports       = [80, 443]
@@ -350,9 +280,9 @@ module "vpc-endpoints" {
     },
     {
       id                  = "ec2messages"
-      vpc_id              = local.env.environment == "prod" ? data.aws_vpc.ct_vpc_id.id : module.vpc[0].vpc_id
+      vpc_id              = data.terraform_remote_state.vpc.outputs.vpc_id
       security_group_ids  = data.aws_security_groups.ecs_sg_list.ids
-      subnet_ids          = local.env.environment == "prod" ? data.aws_subnets.ct_priv_subnets.ids : module.vpc[0].private_subnets
+      subnet_ids          = data.terraform_remote_state.vpc.outputs.private_subnets
       private_dns_enabled = true
       allowed_cidr_blocks = [local.env.cidr]
       inbound_ports       = [80, 443]
@@ -364,9 +294,9 @@ module "vpc-endpoints" {
     },
     {
       id                  = "ssm"
-      vpc_id              = local.env.environment == "prod" ? data.aws_vpc.ct_vpc_id.id : module.vpc[0].vpc_id
+      vpc_id              = data.terraform_remote_state.vpc.outputs.vpc_id
       security_group_ids  = data.aws_security_groups.ecs_sg_list.ids
-      subnet_ids          = local.env.environment == "prod" ? data.aws_subnets.ct_priv_subnets.ids : module.vpc[0].private_subnets
+      subnet_ids          = data.terraform_remote_state.vpc.outputs.private_subnets
       private_dns_enabled = true
       allowed_cidr_blocks = [local.env.cidr]
       inbound_ports       = [80, 443]
@@ -378,9 +308,9 @@ module "vpc-endpoints" {
     },
     {
       id                  = "ssmmessages"
-      vpc_id              = local.env.environment == "prod" ? data.aws_vpc.ct_vpc_id.id : module.vpc[0].vpc_id
+      vpc_id              = data.terraform_remote_state.vpc.outputs.vpc_id
       security_group_ids  = data.aws_security_groups.ecs_sg_list.ids
-      subnet_ids          = local.env.environment == "prod" ? data.aws_subnets.ct_priv_subnets.ids : module.vpc[0].private_subnets
+      subnet_ids          = data.terraform_remote_state.vpc.outputs.private_subnets
       private_dns_enabled = true
       allowed_cidr_blocks = [local.env.cidr]
       inbound_ports       = [80, 443]
