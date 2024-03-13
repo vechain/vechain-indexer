77affef60ff46948fd4aabf54dba2a89f06016db
Dougal Rea <dougal.rea@gmail.com>
2024-03-13 10:10:53 +0000

    Update mongodb.tf

--- terraform/api/mongodb.tf
+++ terraform/api/mongodb.tf
@@ -103,6 +103,7 @@ resource "aws_security_group" "mongodb_sg" {
     from_port       = 27017
     to_port         = 27017
     protocol        = "tcp"
+    # Temporary measure to avoid deployment of new blue/green services from affecting existing prod resources
     security_groups = "${module.ecs-lb-service != null ? [module.ecs-lb-service[each.key].security_group_alb_id, module.ecs-service[each.key].security_group_ecs_service_id] : [module.ecs-lb-service-api[each.key].security_group_alb_id, module.ecs-backend-service[each.key].security_group_ecs_service_id]}"
 
     description = "mongodb service"
