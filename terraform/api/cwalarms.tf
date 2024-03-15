e77d4c6eefaf9f20f4d1c3e6fc69f9f18f37c3e7
Dougal Rea <dougal.rea@gmail.com>
2024-03-15 16:06:46 +0000

    don't create new slack integration

--- terraform/api/cwalarms.tf
+++ terraform/api/cwalarms.tf
@@ -222,7 +222,7 @@ module "cloud_watch_alarms" {
   sns_topic_enabled        = true
   topic_name               = "${local.env.environment}-CloudWatchAlarms"
   email_subscriptions      = []
-  create_slack_integration = true
+  create_slack_integration = startswith(local.env.environment, "prod-") ? false : true
   configuration_name       = substr("${local.env.environment}-${var.project}", 0, 28)
 
   slack_channel_id   = local.env.slack_alert_channel
