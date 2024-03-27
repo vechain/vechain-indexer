defe086e9eaea75a81577c73c8425e99c6b82a89
Dougal Rea <dougal.rea@gmail.com>
2024-03-27 13:43:34 +0000

    Account for missing modules in dev env exports

--- terraform/api/outputs.tf
+++ terraform/api/outputs.tf
@@ -10,19 +10,15 @@ output "test_api_dns_name" {
 
 output "cluster_name" {
   description = "The name of the ECS cluster"
-  value       = module.ecs-cluster[0].name
+  value       = length(module.ecs-cluster) > 0 ? module.ecs-cluster[0].name : ""
 }
 
 output "api_ecs_service_names" {
   description = "The names of the load balanced ECS services"
-  value       = [
-    for service in module.ecs-lb-service-api : service.service_name
-  ]
+  value       = length(module.ecs-lb-service-api) > 0 ? [for service in module.ecs-lb-service-api : service.service_name] : []
 }
 
 output "indexer_ecs_service_names" {
   description = "The names of the backend ECS services"
-  value       = [
-    for service in module.ecs-backend-service : service.service_name
-  ]
+  value       = length(module.ecs-backend-service) > 0 ? [for service in module.ecs-backend-service : service.service_name] : []
 }
