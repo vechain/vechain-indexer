e4fd72e8560c9a30444ab9b6b7f83afd0b6a2627
Dougal Rea <dougal.rea@gmail.com>
2024-04-10 15:43:48 +0100

    Populate CW Dashboard & Improve Readability

--- terraform/api/outputs.tf
+++ terraform/api/outputs.tf
@@ -1,13 +1,3 @@
-output "main_api_dns_name" {
-  description = "The DNS name of the mainnet API"
-  value       = length(module.ecs-lb-service-api) > 0 ? module.ecs-lb-service-api["main"].alb_dns_name : ""
-}
-
-output "test_api_dns_name" {
-  description = "The DNS name of the testnet API"
-  value       = length(module.ecs-lb-service-api) > 0 ? module.ecs-lb-service-api["test"].alb_dns_name : ""
-}
-
 output "cluster_name" {
   description = "The name of the ECS cluster"
   value       = length(module.ecs-cluster) > 0 ? module.ecs-cluster[0].name : ""
