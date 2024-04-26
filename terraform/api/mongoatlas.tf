56744cea1d0debc83c6327ec6da8a43788cb10d8
Dougal Rea <dougal.rea@gmail.com>
2024-04-26 10:48:10 +0100

    parameterise slack secret

--- terraform/api/mongoatlas.tf
+++ terraform/api/mongoatlas.tf
@@ -5,7 +5,7 @@ module "mongoatlas-main-net" {
   project_id = local.env.mongoatlas_project_id # MongoDB Atlas project ID
 
   create_api_key = false
-  slack_api_token = "arn:aws:secretsmanager:eu-west-1:905964754131:secret:veworld-mongodb-alerts/slack/api-token-NvxUZd"
+  slack_api_token = local.env.slack_secret_arn
   alerts = {
     alert_type_1 = {
       event_type = "HOST_MONGOT_CRASHING_OOM"
@@ -107,7 +107,7 @@ module "mongoatlas-test-net" {
   project_id = local.env.mongoatlas_project_id # MongoDB Atlas project ID
 
   create_api_key = false
-  slack_api_token = "arn:aws:secretsmanager:eu-west-1:905964754131:secret:veworld-mongodb-alerts/slack/api-token-NvxUZd"
+  slack_api_token = local.env.slack_secret_arn
   alerts = {
     alert_type_1 = {
       event_type = "HOST_MONGOT_CRASHING_OOM"
