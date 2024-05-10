7523c691bf7b9302b45275fd57c0f1ec05ab753e
Daithi Hearn <daithi.hearn@gmail.com>
2024-05-10 11:06:26 +0200

    test: minor improvements to unit tests

--- packages/indexer/src/test/kotlin/org/vechain/indexer/utils/BlockUtilsTest.kt
+++ packages/indexer/src/test/kotlin/org/vechain/indexer/utils/BlockUtilsTest.kt
@@ -2,10 +2,11 @@ package org.vechain.indexer.utils
 
 import org.junit.jupiter.api.Test
 import org.vechain.indexer.fixtures.BlockFixtures
+import org.vechain.indexer.model.IndexedTransferEvent
 import org.vechain.indexer.model.TransferEventType
+import strikt.api.expect
 import strikt.api.expectThat
-import strikt.assertions.isEqualTo
-import strikt.assertions.isTrue
+import strikt.assertions.*
 
 class BlockUtilsTest {
 
@@ -14,19 +15,20 @@ class BlockUtilsTest {
         val block = BlockFixtures.BLOCK_NFT_MINT
         val nftTransferEvents = BlockUtils.getNftTransferEventsFromTopics(block, null)
 
-        expectThat(nftTransferEvents.size).isEqualTo(1)
-        expectThat(nftTransferEvents[0].eventType).isEqualTo(TransferEventType.NFT)
-        expectThat(nftTransferEvents[0].tokenAddress)
-            .isEqualTo("0x755a8cada7bbdc22b7413f40931389fdfe2b9089")
-        expectThat(nftTransferEvents[0].from)
-            .isEqualTo("0x0000000000000000000000000000000000000000")
-        expectThat(nftTransferEvents[0].to).isEqualTo("0xf077b491b355e64048ce21e3a6fc4751eeea77fa")
-        expectThat(nftTransferEvents[0].tokenId).isEqualTo("0")
-        expectThat(nftTransferEvents[0].value).isEqualTo("1")
-        expectThat(nftTransferEvents[0].blockNumber).isEqualTo(block.number)
-        expectThat(nftTransferEvents[0].blockTimestamp).isEqualTo(block.timestamp)
-        expectThat(nftTransferEvents[0].txId).isEqualTo(block.transactions[0].id)
-        expectThat(nftTransferEvents[0].topics.size).isEqualTo(4)
+        expect {
+            that(nftTransferEvents.size).isEqualTo(1)
+            that(nftTransferEvents[0].eventType).isEqualTo(TransferEventType.NFT)
+            that(nftTransferEvents[0].tokenAddress)
+                .isEqualTo("0x755a8cada7bbdc22b7413f40931389fdfe2b9089")
+            that(nftTransferEvents[0].from).isEqualTo("0x0000000000000000000000000000000000000000")
+            that(nftTransferEvents[0].to).isEqualTo("0xf077b491b355e64048ce21e3a6fc4751eeea77fa")
+            that(nftTransferEvents[0].tokenId).isEqualTo("0")
+            that(nftTransferEvents[0].value).isEqualTo("1")
+            that(nftTransferEvents[0].blockNumber).isEqualTo(block.number)
+            that(nftTransferEvents[0].blockTimestamp).isEqualTo(block.timestamp)
+            that(nftTransferEvents[0].txId).isEqualTo(block.transactions[0].id)
+            that(nftTransferEvents[0].topics.size).isEqualTo(4)
+        }
     }
 
     @Test
@@ -34,32 +36,34 @@ class BlockUtilsTest {
         val block = BlockFixtures.BLOCK_NFT_MINT_2
         val nftTransferEvents = BlockUtils.getNftTransferEventsFromTopics(block, null)
 
-        expectThat(nftTransferEvents.size).isEqualTo(2)
-        expectThat(nftTransferEvents[0].eventType).isEqualTo(TransferEventType.NFT)
-        expectThat(nftTransferEvents[0].tokenAddress)
-            .isEqualTo("0xed324c3628923d9816012cb5bc10c4d817e824a5")
-        expectThat(nftTransferEvents[0].from)
-            .isEqualTo("0x0000000000000000000000000000000000000000")
-        expectThat(nftTransferEvents[0].to).isEqualTo("0x435933c8064b4ae76be665428e0307ef2ccfbd68")
-        expectThat(nftTransferEvents[0].tokenId).isEqualTo("0")
-        expectThat(nftTransferEvents[0].value).isEqualTo("1")
-        expectThat(nftTransferEvents[0].blockNumber).isEqualTo(block.number)
-        expectThat(nftTransferEvents[0].blockTimestamp).isEqualTo(block.timestamp)
-        expectThat(nftTransferEvents[0].txId).isEqualTo(block.transactions[0].id)
-        expectThat(nftTransferEvents[0].topics.size).isEqualTo(4)
+        expect {
+            that(nftTransferEvents.size).isEqualTo(2)
+            that(nftTransferEvents[0].eventType).isEqualTo(TransferEventType.NFT)
+            that(nftTransferEvents[0].tokenAddress)
+                .isEqualTo("0xed324c3628923d9816012cb5bc10c4d817e824a5")
+            that(nftTransferEvents[0].from).isEqualTo("0x0000000000000000000000000000000000000000")
+            that(nftTransferEvents[0].to).isEqualTo("0x435933c8064b4ae76be665428e0307ef2ccfbd68")
+            that(nftTransferEvents[0].tokenId).isEqualTo("0")
+            that(nftTransferEvents[0].value).isEqualTo("1")
+            that(nftTransferEvents[0].blockNumber).isEqualTo(block.number)
+            that(nftTransferEvents[0].blockTimestamp).isEqualTo(block.timestamp)
+            that(nftTransferEvents[0].txId).isEqualTo(block.transactions[0].id)
+            that(nftTransferEvents[0].topics.size).isEqualTo(4)
+        }
 
-        expectThat(nftTransferEvents[1].eventType).isEqualTo(TransferEventType.NFT)
-        expectThat(nftTransferEvents[1].tokenAddress)
-            .isEqualTo("0xed324c3628923d9816012cb5bc10c4d817e824a5")
-        expectThat(nftTransferEvents[1].from)
-            .isEqualTo("0x0000000000000000000000000000000000000000")
-        expectThat(nftTransferEvents[1].to).isEqualTo("0xf077b491b355e64048ce21e3a6fc4751eeea77fa")
-        expectThat(nftTransferEvents[1].tokenId).isEqualTo("1")
-        expectThat(nftTransferEvents[1].value).isEqualTo("1")
-        expectThat(nftTransferEvents[1].blockNumber).isEqualTo(block.number)
-        expectThat(nftTransferEvents[1].blockTimestamp).isEqualTo(block.timestamp)
-        expectThat(nftTransferEvents[1].txId).isEqualTo(block.transactions[1].id)
-        expectThat(nftTransferEvents[1].topics.size).isEqualTo(4)
+        expect {
+            that(nftTransferEvents[1].eventType).isEqualTo(TransferEventType.NFT)
+            that(nftTransferEvents[1].tokenAddress)
+                .isEqualTo("0xed324c3628923d9816012cb5bc10c4d817e824a5")
+            that(nftTransferEvents[1].from).isEqualTo("0x0000000000000000000000000000000000000000")
+            that(nftTransferEvents[1].to).isEqualTo("0xf077b491b355e64048ce21e3a6fc4751eeea77fa")
+            that(nftTransferEvents[1].tokenId).isEqualTo("1")
+            that(nftTransferEvents[1].value).isEqualTo("1")
+            that(nftTransferEvents[1].blockNumber).isEqualTo(block.number)
+            that(nftTransferEvents[1].blockTimestamp).isEqualTo(block.timestamp)
+            that(nftTransferEvents[1].txId).isEqualTo(block.transactions[1].id)
+            that(nftTransferEvents[1].topics.size).isEqualTo(4)
+        }
     }
 
     @Test
@@ -72,18 +76,24 @@ class BlockUtilsTest {
             )
 
         expectThat(nftTransferEvents.size).isEqualTo(1)
-        expectThat(nftTransferEvents[0].eventType).isEqualTo(TransferEventType.NFT)
-        expectThat(nftTransferEvents[0].tokenAddress)
-            .isEqualTo("0x755a8cada7bbdc22b7413f40931389fdfe2b9089")
-        expectThat(nftTransferEvents[0].from)
-            .isEqualTo("0x0000000000000000000000000000000000000000")
-        expectThat(nftTransferEvents[0].to).isEqualTo("0xf077b491b355e64048ce21e3a6fc4751eeea77fa")
-        expectThat(nftTransferEvents[0].tokenId).isEqualTo("0")
-        expectThat(nftTransferEvents[0].value).isEqualTo("1")
-        expectThat(nftTransferEvents[0].blockNumber).isEqualTo(block.number)
-        expectThat(nftTransferEvents[0].blockTimestamp).isEqualTo(block.timestamp)
-        expectThat(nftTransferEvents[0].txId).isEqualTo(block.transactions[0].id)
-        expectThat(nftTransferEvents[0].topics.size).isEqualTo(4)
+
+        expect {
+            that(nftTransferEvents[0].eventType).isEqualTo(TransferEventType.NFT)
+            that(nftTransferEvents[0].tokenAddress)
+                .isEqualTo("0x755a8cada7bbdc22b7413f40931389fdfe2b9089")
+            that(nftTransferEvents[0].from).isEqualTo("0x0000000000000000000000000000000000000000")
+            that(nftTransferEvents[0].to).isEqualTo("0xf077b491b355e64048ce21e3a6fc4751eeea77fa")
+            that(nftTransferEvents[0].tokenId).isEqualTo("0")
+            that(nftTransferEvents[0].value).isEqualTo("1")
+            that(nftTransferEvents[0].blockNumber).isEqualTo(block.number)
+            that(nftTransferEvents[0].blockTimestamp).isEqualTo(block.timestamp)
+            that(nftTransferEvents[0].txId).isEqualTo(block.transactions[0].id)
+            that(nftTransferEvents[0].topics.size).isEqualTo(4)
+        }
+
+        expectThat(nftTransferEvents).map(IndexedTransferEvent::tokenAddress).all {
+            isEqualTo("0x755a8cada7bbdc22b7413f40931389fdfe2b9089")
+        }
     }
 
     @Test
@@ -95,7 +105,7 @@ class BlockUtilsTest {
                 "0xf077b491b355e64048ce21e3a6fc4751eeea77fa"
             )
 
-        expectThat(nftTransferEvents.isEmpty()).isTrue()
+        expectThat(nftTransferEvents).isEmpty()
     }
 
     @Test
@@ -104,8 +114,10 @@ class BlockUtilsTest {
         val outputs = BlockUtils.getOutputs(block)
 
         expectThat(outputs.size).isEqualTo(1)
-        expectThat(outputs[0].first.events.size).isEqualTo(2)
-        expectThat(outputs[0].second.id).isEqualTo(block.transactions[0].id)
+        expect {
+            that(outputs[0].first.events.size).isEqualTo(2)
+            that(outputs[0].second.id).isEqualTo(block.transactions[0].id)
+        }
     }
 
     @Test
@@ -114,55 +126,60 @@ class BlockUtilsTest {
         val outputs = BlockUtils.getOutputs(block)
 
         expectThat(outputs.size).isEqualTo(2)
-        expectThat(outputs[0].first.events.size).isEqualTo(2)
-        expectThat(outputs[0].first.events[0].address)
-            .isEqualTo("0xed324c3628923d9816012cb5bc10c4d817e824a5")
-        expectThat(outputs[0].first.events[0].topics.size).isEqualTo(2)
-        expectThat(outputs[0].first.events[0].topics[0])
-            .isEqualTo("0x26e57b75d51bb4535260f2bbcf7f5ca9f9612af5e3b52e82d20852b529f03290")
-        expectThat(outputs[0].first.events[0].topics[1])
-            .isEqualTo("0x000000000000000000000000435933c8064b4ae76be665428e0307ef2ccfbd68")
-        expectThat(outputs[0].first.events[0].data)
-            .isEqualTo(
-                "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001"
-            )
-        expectThat(outputs[0].first.events[1].address)
-            .isEqualTo("0xed324c3628923d9816012cb5bc10c4d817e824a5")
-        expectThat(outputs[0].first.events[1].topics.size).isEqualTo(4)
-        expectThat(outputs[0].first.events[1].topics[0])
-            .isEqualTo("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef")
-        expectThat(outputs[0].first.events[1].topics[1])
-            .isEqualTo("0x0000000000000000000000000000000000000000000000000000000000000000")
-        expectThat(outputs[0].first.events[1].topics[2])
-            .isEqualTo("0x000000000000000000000000435933c8064b4ae76be665428e0307ef2ccfbd68")
-        expectThat(outputs[0].first.events[1].topics[3])
-            .isEqualTo("0x0000000000000000000000000000000000000000000000000000000000000000")
-        expectThat(outputs[0].second.id).isEqualTo(block.transactions[0].id)
 
-        expectThat(outputs[1].first.events.size).isEqualTo(2)
-        expectThat(outputs[1].first.events[0].address)
-            .isEqualTo("0xed324c3628923d9816012cb5bc10c4d817e824a5")
-        expectThat(outputs[1].first.events[0].topics.size).isEqualTo(2)
-        expectThat(outputs[1].first.events[0].topics[0])
-            .isEqualTo("0x26e57b75d51bb4535260f2bbcf7f5ca9f9612af5e3b52e82d20852b529f03290")
-        expectThat(outputs[1].first.events[0].topics[1])
-            .isEqualTo("0x000000000000000000000000f077b491b355e64048ce21e3a6fc4751eeea77fa")
-        expectThat(outputs[1].first.events[0].data)
-            .isEqualTo(
-                "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001"
-            )
-        expectThat(outputs[1].first.events[1].address)
-            .isEqualTo("0xed324c3628923d9816012cb5bc10c4d817e824a5")
-        expectThat(outputs[1].first.events[1].topics.size).isEqualTo(4)
-        expectThat(outputs[1].first.events[1].topics[0])
-            .isEqualTo("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef")
-        expectThat(outputs[1].first.events[1].topics[1])
-            .isEqualTo("0x0000000000000000000000000000000000000000000000000000000000000000")
-        expectThat(outputs[1].first.events[1].topics[2])
-            .isEqualTo("0x000000000000000000000000f077b491b355e64048ce21e3a6fc4751eeea77fa")
-        expectThat(outputs[1].first.events[1].topics[3])
-            .isEqualTo("0x0000000000000000000000000000000000000000000000000000000000000001")
-        expectThat(outputs[1].second.id).isEqualTo(block.transactions[1].id)
+        expect {
+            that(outputs[0].first.events.size).isEqualTo(2)
+            that(outputs[0].first.events[0].address)
+                .isEqualTo("0xed324c3628923d9816012cb5bc10c4d817e824a5")
+            that(outputs[0].first.events[0].topics.size).isEqualTo(2)
+            that(outputs[0].first.events[0].topics[0])
+                .isEqualTo("0x26e57b75d51bb4535260f2bbcf7f5ca9f9612af5e3b52e82d20852b529f03290")
+            that(outputs[0].first.events[0].topics[1])
+                .isEqualTo("0x000000000000000000000000435933c8064b4ae76be665428e0307ef2ccfbd68")
+            that(outputs[0].first.events[0].data)
+                .isEqualTo(
+                    "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001"
+                )
+            that(outputs[0].first.events[1].address)
+                .isEqualTo("0xed324c3628923d9816012cb5bc10c4d817e824a5")
+            that(outputs[0].first.events[1].topics.size).isEqualTo(4)
+            that(outputs[0].first.events[1].topics[0])
+                .isEqualTo("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef")
+            that(outputs[0].first.events[1].topics[1])
+                .isEqualTo("0x0000000000000000000000000000000000000000000000000000000000000000")
+            that(outputs[0].first.events[1].topics[2])
+                .isEqualTo("0x000000000000000000000000435933c8064b4ae76be665428e0307ef2ccfbd68")
+            that(outputs[0].first.events[1].topics[3])
+                .isEqualTo("0x0000000000000000000000000000000000000000000000000000000000000000")
+            that(outputs[0].second.id).isEqualTo(block.transactions[0].id)
+        }
+
+        expect {
+            that(outputs[1].first.events.size).isEqualTo(2)
+            that(outputs[1].first.events[0].address)
+                .isEqualTo("0xed324c3628923d9816012cb5bc10c4d817e824a5")
+            that(outputs[1].first.events[0].topics.size).isEqualTo(2)
+            that(outputs[1].first.events[0].topics[0])
+                .isEqualTo("0x26e57b75d51bb4535260f2bbcf7f5ca9f9612af5e3b52e82d20852b529f03290")
+            that(outputs[1].first.events[0].topics[1])
+                .isEqualTo("0x000000000000000000000000f077b491b355e64048ce21e3a6fc4751eeea77fa")
+            that(outputs[1].first.events[0].data)
+                .isEqualTo(
+                    "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001"
+                )
+            that(outputs[1].first.events[1].address)
+                .isEqualTo("0xed324c3628923d9816012cb5bc10c4d817e824a5")
+            that(outputs[1].first.events[1].topics.size).isEqualTo(4)
+            that(outputs[1].first.events[1].topics[0])
+                .isEqualTo("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef")
+            that(outputs[1].first.events[1].topics[1])
+                .isEqualTo("0x0000000000000000000000000000000000000000000000000000000000000000")
+            that(outputs[1].first.events[1].topics[2])
+                .isEqualTo("0x000000000000000000000000f077b491b355e64048ce21e3a6fc4751eeea77fa")
+            that(outputs[1].first.events[1].topics[3])
+                .isEqualTo("0x0000000000000000000000000000000000000000000000000000000000000001")
+            that(outputs[1].second.id).isEqualTo(block.transactions[1].id)
+        }
     }
 
     @Test
@@ -172,7 +189,10 @@ class BlockUtilsTest {
         val outputs = BlockUtils.getOutputs(block)
 
         expectThat(outputs.size).isEqualTo(1)
-        expectThat(outputs[0].first.events.size).isEqualTo(2)
-        expectThat(outputs[0].second.id).isEqualTo(block.transactions[1].id)
+
+        expect {
+            that(outputs[0].first.events.size).isEqualTo(2)
+            that(outputs[0].second.id).isEqualTo(block.transactions[1].id)
+        }
     }
 }
