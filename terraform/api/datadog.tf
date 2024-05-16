b397bf05d05b39e93a8b2bf0a3d43534827b0404
ShaunKav <shaun.kavanagh@vechain.org>
2024-05-16 13:41:39 +0100

    reverting region setting through namespace

--- terraform/api/datadog.tf
+++ terraform/api/datadog.tf
@@ -47,10 +47,7 @@ module "datadog_integration_aws" {
   
   filter_tags = []
   host_tags   = [" Env:${local.env.project}"]
-  namespace_rules = {
-    "us-east-1" = true  // Enable monitoring for us-east-1
-    "eu-west-1" = true  // Enable monitoring for us-east-1
-  }
+  excluded_regions = ["us-east-2, us-west-1, us-west-2, ca-central-1, eu-west-2, eu-west-3, eu-central-1, eu-north-1, ap-south-1, ap-northeast-1, ap-northeast-2, ap-southeast-1, ap-southeast-2, sa-east-1"]
 
   dashboard_title       = "${local.env.project} Dashboard"
   dashboard_description = "Monitoring dashboard for ${local.env.project}"
