1cd1424f9936c23d2b89da9d392df547de6a4f5a
WaqarAhmedVC <122815219+WaqarAhmedVC@users.noreply.github.com>
2024-05-20 09:08:48 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,15 @@ plugins {
     id("com.diffplug.spotless") version "6.25.0"
     id("org.sonarqube") version "4.4.1.3373"
     jacoco
+    id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "vechain_veworld-indexer")
+    property("sonar.organization", "vechain")
+    property("sonar.host.url", "https://sonarcloud.io")
+  }
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_17
