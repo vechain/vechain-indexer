cf651006cacbbdf50032e549973fd8b90d8b4659
Dougal Rea <dougal.rea@gmail.com>
2024-06-12 10:59:50 +0100

    fix vars

--- terraform/vpc/slack_chatbot.tf
+++ terraform/vpc/slack_chatbot.tf
@@ -12,7 +12,7 @@ resource "awscc_chatbot_slack_channel_configuration" "slack_integration" {
   slack_workspace_id = local.env.slack_workspace_id
   iam_role_arn       = aws_iam_role.cloudwatch_read.arn
   guardrail_policies = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
-  sns_topic_arns     = aws_sns_topic.sns_topic.arn
+  sns_topic_arns     = [aws_sns_topic.sns_topic.arn]
 }
 
 # IAM Resources for CloudWatch Alarms
