06751fa2e10bd61b4dbc493543014a6f41746eaf
Luc Grandin <104393342+GrandinLuc@users.noreply.github.com>
2024-11-19 10:35:14 +0100

    fix: re-commented terraform version

--- terraform/vpc/provider.tf
+++ terraform/vpc/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.35"
+      #version = "3.75.1"
     }
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
