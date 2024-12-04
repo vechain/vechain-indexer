e112d43f1d05f0aa9891ee12cf9eb1d5f452e0af
Daithi Hearn <daithi.hearn@gmail.com>
2024-12-04 23:01:09 +0100

    removing duplicate default values as it may lead to confusion

--- packages/indexer/src/main/kotlin/org/vechain/indexer/StatefulIndexer.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/StatefulIndexer.kt
@@ -8,11 +8,11 @@ import org.vechain.indexer.thor.client.ThorClient
 
 abstract class StatefulIndexer<T : VersionedDocument, S : Archive<T>>(
     repository: BaseIndexedRepository<*>,
-    startBlock: Long = 0L,
+    startBlock: Long,
     thorClient: ThorClient,
-    syncLogInterval: Long = 1000L,
-    private val prunerEnabled: Boolean = false,
-    private val prunerInterval: Long = 1000L,
+    syncLogInterval: Long,
+    private val prunerEnabled: Boolean,
+    private val prunerInterval: Long,
     private val archiveService: ArchiveService<T, S>
 ) :
     BaseIndexer(
