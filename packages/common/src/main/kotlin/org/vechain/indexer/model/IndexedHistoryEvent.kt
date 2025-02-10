8b92f3cdd13871e707cb9dfdb330dc3219132c3b
roisindowling <roisin.dowling@vechain.org>
2025-02-10 14:19:16 +1100

    Add history event enum and data classes

--- packages/common/src/main/kotlin/org/vechain/indexer/model/IndexedHistoryEvent.kt
+++ packages/common/src/main/kotlin/org/vechain/indexer/model/IndexedHistoryEvent.kt
@@ -1,50 +1,16 @@
 package org.vechain.indexer.model
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import org.springframework.boot.context.properties.bind.ConstructorBinding
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
-
-enum class HistoryEventName {
-    B3TR_SWAP_VOT3_TO_B3TR,
-    B3TR_SWAP_B3TR_TO_VOT3,
-    B3TR_PROPOSAL_SUPPORT,
-    B3TR_CLAIM_REWARD,
-    B3TR_UPGRADE_GM,
-    B3TR_ACTION,
-    B3TR_PROPOSAL_VOTE,
-    B3TR_XALLOCATION_VOTE,
-    TRANSFER_VET,
-    TRANSFER_FT,
-    TRANSFER_NFT,
-    TRANSFER_SF,
-    SWAP_VET_TO_FT,
-    SWAP_FT_TO_VET,
-    SWAP_FT_TO_FT,
-}
-
-enum class HistoryEventType {
-    B3TR,
-    TRANSFER,
-    SWAP,
-    GENERIC_TX,
-}
-
-enum class ProposalSupport(val value: Int) {
-    AGAINST(0),
-    FOR(1),
-    ABSTAIN(2);
-
-    companion object {
-        fun fromValue(value: Int?): ProposalSupport? {
-            return values().find { it.value == value }
-        }
-    }
-}
-
-/** Data class representing an App ID and its corresponding vote weight. */
-data class AppVote(val appId: String, val voteWeight: String)
+import org.vechain.indexer.model.b3tr.AppVote
+import org.vechain.indexer.model.b3tr.ProposalSupport
+import org.vechain.indexer.model.history.HistoryEventName
+import org.vechain.indexer.model.history.HistoryEventType
 
 @Document(collection = "history_events")
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class IndexedHistoryEvent
 @ConstructorBinding
 constructor(
@@ -57,8 +23,6 @@ constructor(
     val gasPayer: String? = null,
     val contractAddress: String? = null,
     val tokenId: String? = null,
-    val topics: List<String>? = null,
-    val data: String? = null,
     val eventName: HistoryEventName? = null,
     val eventType: HistoryEventType,
     val to: String? = null,
@@ -80,9 +44,11 @@ constructor(
     val inputValue: String? = null,
     val outputValue: String? = null,
 ) : IndexedDocument {
-
     companion object {
-        fun getAppVotes(appIds: Any?, voteWeights: Any?): List<AppVote>? {
+        fun getAppVotes(
+            appIds: Any?,
+            voteWeights: Any?,
+        ): List<AppVote>? {
             // Ensure both are non-null and cast to List<String>
             val appIdsList = appIds as? List<*> ?: return null
             val voteWeightsList = voteWeights as? List<*> ?: return null
---
+++ packages/common/src/main/kotlin/org/vechain/indexer/model/history/HistoryEventType.kt
@@ -0,0 +1,8 @@
+package org.vechain.indexer.model.history
+
+enum class HistoryEventType {
+    B3TR,
+    TRANSFER,
+    SWAP,
+    GENERIC_TX,
+}
