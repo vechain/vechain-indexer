b82dc0eed39045f087e07f8d102a1bbda919d6cc
roisindowling <roisin.dowling@vechain.org>
2025-02-11 12:44:19 +1100

    Add decoded events to TX indexer

--- packages/indexer/src/main/kotlin/org/vechain/indexer/TransactionIndexer.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/TransactionIndexer.kt
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Profile
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.stereotype.Component
+import org.vechain.indexer.event.AbiManager
+import org.vechain.indexer.event.BusinessEventManager
 import org.vechain.indexer.model.IndexedTransaction
 import org.vechain.indexer.repository.TransactionRepository
+import org.vechain.indexer.service.TransactionService
 import org.vechain.indexer.thor.client.ThorClient
 import org.vechain.indexer.thor.model.Block
 
@@ -13,8 +16,11 @@ import org.vechain.indexer.thor.model.Block
 @Component
 open class TransactionIndexer(
     private val txRepo: TransactionRepository,
+    private val transactionService: TransactionService,
     private val mongoTemplate: MongoTemplate,
     thorClient: ThorClient,
+    abiManager: AbiManager,
+    businessEventManager: BusinessEventManager,
     @Value("\${indexer.startBlock.transactions}") private val startBlock: Long,
     @Value("\${indexer.syncLogInterval.transactions}") private val syncLogInterval: Long,
 ) :
@@ -22,19 +28,24 @@ open class TransactionIndexer(
         repository = txRepo,
         startBlock = startBlock,
         thorClient = thorClient,
-        syncLogInterval = syncLogInterval
+        syncLogInterval = syncLogInterval,
+        abiManager = abiManager,
+        businessEventManager = businessEventManager,
     ) {
-
     override fun rollback(blockNumber: Long) {
         txRepo.deleteAllByBlockNumberBetween(blockNumber - 1, blockNumber + 1)
     }
 
     override fun processBlock(block: Block) {
-        if (block.transactions.isNotEmpty()) {
-            mongoTemplate.insert(
-                block.transactions.map { IndexedTransaction(block, it) },
-                IndexedTransaction::class.java
+        val eventsByTx = processBlockGenericEvents(block).groupBy { it.first.txId }
+
+        val transactions =
+            transactionService.processBlockTransactions(
+                block.transactions,
+                eventsByTx,
+                block,
             )
-        }
+
+        mongoTemplate.insert(transactions, IndexedTransaction::class.java)
     }
 }
