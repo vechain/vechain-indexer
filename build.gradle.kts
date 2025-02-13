68c54766bed0d59113a85ddf54cf650261a4fbc5
roisindowling <roisin.dowling@vechain.org>
2025-02-13 21:42:52 +1100

    Use latest snapshot version of indexer core

--- build.gradle.kts
+++ build.gradle.kts
@@ -243,7 +243,7 @@ allprojects {
         implementation("commons-codec:commons-codec:1.15")
 
         // Core indexer dependency
-        implementation("org.vechain:indexer-core:2.1.0-SNAPSHOT")
+        implementation("org.vechain:indexer-core:2.1.1-SNAPSHOT")
 
         // Test dependencies
         testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.1")
--- packages/indexer/src/main/kotlin/org/vechain/indexer/config/IndexerConfig.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/config/IndexerConfig.kt
@@ -7,30 +7,31 @@ import org.vechain.indexer.event.AbiManager
 import org.vechain.indexer.event.BusinessEventManager
 import org.vechain.indexer.thor.client.DefaultThorClient
 import org.vechain.indexer.thor.client.ThorClient
+import org.vechain.indexer.utils.FileUtils
 
 @Configuration
 open class IndexerConfig(
     @Value("\${thor.url}") private val thorUrl: String,
     @Value("\${abis.path}") private val abisPath: String,
-    @Value("\${businessEvents.path}") private val businessEventsPath: String
+    @Value("\${businessEvents.path}") private val businessEventsPath: String,
 ) {
-
     @Bean
-    open fun thorClient(): ThorClient {
-        return DefaultThorClient(thorUrl, Pair("X-Project-Id", "veworld-indexer"))
-    }
+    open fun thorClient(): ThorClient =
+        DefaultThorClient(thorUrl, Pair("X-Project-Id", "veworld-indexer"))
 
     @Bean
     open fun abiManager(): AbiManager {
         val abiManager = AbiManager()
-        abiManager.loadAbis(abisPath)
+        val abis = FileUtils.loadFileStreams(abisPath)
+        abiManager.loadAbis(abis)
         return abiManager
     }
 
     @Bean
     open fun businessEventManager(): BusinessEventManager {
         val businessEventManager = BusinessEventManager()
-        businessEventManager.loadBusinessEvents(businessEventsPath)
+        val businessEvents = FileUtils.loadFileStreams(businessEventsPath)
+        businessEventManager.loadBusinessEvents(businessEvents)
         return businessEventManager
     }
 }
