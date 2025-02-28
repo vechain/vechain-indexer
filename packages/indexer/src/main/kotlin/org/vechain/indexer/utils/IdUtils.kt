45ea414473112ac8b86a0c9e88355dc1fe5822d0
roisindowling <roisin.dowling@vechain.org>
2025-02-28 19:47:21 +1100

    Add ERC721 ABI

--- packages/indexer/src/main/kotlin/org/vechain/indexer/utils/IdUtils.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/utils/IdUtils.kt
@@ -1,18 +1,20 @@
 package org.vechain.indexer.utils
 
 import org.apache.commons.codec.digest.DigestUtils
-import org.vechain.indexer.model.IndexedTransferEvent
+import org.vechain.indexer.event.model.generic.GenericEventParameters
+import org.vechain.indexer.event.model.generic.IndexedEvent
 import org.vechain.indexer.model.VersionedDocument
-import org.web3j.utils.Numeric
+import org.vechain.indexer.utils.ParamUtils.getAsString
 
 object IdUtils {
+    fun buildArchiveId(
+        document: VersionedDocument,
+        version: Int = document.version,
+    ): String = buildHashedId("${document::class.simpleName}-${document.getDocumentId()}-$version")
 
-    fun buildArchiveId(document: VersionedDocument, version: Int = document.version): String =
-        buildHashedId("${document::class.simpleName}-${document.getDocumentId()}-${version}")
-
-    fun buildNftId(transferEvent: IndexedTransferEvent): String =
+    fun buildNftId(transferEvent: Pair<IndexedEvent, GenericEventParameters>): String =
         buildHashedId(
-            "${transferEvent.tokenAddress}-${Numeric.parsePaddedNumberHex(transferEvent.topics[3])}"
+            "${transferEvent.first.address}-${transferEvent.second.params.getAsString("tokenId")}",
         )
 }
 
