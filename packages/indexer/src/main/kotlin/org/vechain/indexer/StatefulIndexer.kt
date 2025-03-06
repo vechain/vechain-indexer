a1e766a56e4a1a8721b420801603e7ffbf9edd24
roisindowling <roisin.dowling@vechain.org>
2025-03-06 16:03:34 +0700

    Add Pruner Service

--- packages/indexer/src/main/kotlin/org/vechain/indexer/StatefulIndexer.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/StatefulIndexer.kt
@@ -4,6 +4,7 @@ import org.vechain.indexer.model.Archive
 import org.vechain.indexer.model.VersionedDocument
 import org.vechain.indexer.repository.BaseIndexedRepository
 import org.vechain.indexer.service.ArchiveService
+import org.vechain.indexer.service.PrunerService
 import org.vechain.indexer.thor.client.ThorClient
 
 abstract class StatefulIndexer<T : VersionedDocument, S : Archive<T>>(
@@ -12,44 +13,20 @@ abstract class StatefulIndexer<T : VersionedDocument, S : Archive<T>>(
     thorClient: ThorClient,
     syncLogInterval: Long = 1000L,
     private val prunerRemovalChunkSize: Int,
-    private val archiveService: ArchiveService<T, S>
+    private val archiveService: ArchiveService<T, S>,
+    private val prunerService: PrunerService<T, S>,
 ) :
     BaseIndexer(
         repository = repository,
         startBlock = startBlock,
         thorClient = thorClient,
-        syncLogInterval = syncLogInterval
+        syncLogInterval = syncLogInterval,
     ) {
-
     override fun rollback(blockNumber: Long) {
         archiveService.rollback(blockNumber)
     }
 
     fun runPruner() {
-        // Only run the pruner if the indexer is synced
-        if (this.status != Status.FULLY_SYNCED) {
-            logger.info("Skipping pruner for ${this.name} as not fully synced")
-            return
-        }
-
-        // Assume the block 10,000 blocks ago is finalised and a safe point to prune to
-        val prunerEndBlock = this.currentBlockNumber - 10_000
-        if (prunerEndBlock <= 0) {
-            logger.info("Skipping pruner for ${this.name} as not enough blocks to prune")
-            return
-        }
-
-        // Get the records to prune
-        val records = archiveService.findRecordsToPrune(prunerEndBlock)
-        if (records.isEmpty()) {
-            logger.info("No records to prune for ${this.name}")
-            return
-        }
-
-        // Prune the records in chunks to avoid memory issues
-        logger.info(
-            "Pruning ${records.size} records for ${this.name} (in chunks of ${prunerRemovalChunkSize})"
-        )
-        records.chunked(prunerRemovalChunkSize).forEach { chunk -> archiveService.removeAll(chunk) }
+        prunerService.runPruner(this.name, this.currentBlockNumber, this.status)
     }
 }
--- packages/indexer/src/main/kotlin/org/vechain/indexer/StatefulLogsIndexer.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/StatefulLogsIndexer.kt
@@ -6,6 +6,7 @@ import org.vechain.indexer.model.Archive
 import org.vechain.indexer.model.VersionedDocument
 import org.vechain.indexer.repository.BaseIndexedRepository
 import org.vechain.indexer.service.ArchiveService
+import org.vechain.indexer.service.PrunerService
 import org.vechain.indexer.thor.client.ThorClient
 import org.vechain.indexer.thor.enums.LogType
 
@@ -18,8 +19,8 @@ abstract class StatefulLogsIndexer<T : VersionedDocument, S : Archive<T>>(
     logsType: Set<LogType>,
     abiManager: AbiManager?,
     businessEventManager: BusinessEventManager?,
-    private val prunerRemovalChunkSize: Int,
     private val archiveService: ArchiveService<T, S>,
+    private val prunerService: PrunerService<T, S>,
 ) :
     BaseLogIndexer(
         repository = repository,
@@ -36,30 +37,6 @@ abstract class StatefulLogsIndexer<T : VersionedDocument, S : Archive<T>>(
     }
 
     fun runPruner() {
-        // Only run the pruner if the indexer is synced
-        if (this.status != Status.FULLY_SYNCED) {
-            logger.info("Skipping pruner for ${this.name} as not fully synced")
-            return
-        }
-
-        // Assume the block 10,000 blocks ago is finalised and a safe point to prune to
-        val prunerEndBlock = this.currentBlockNumber - 10_000
-        if (prunerEndBlock <= 0) {
-            logger.info("Skipping pruner for ${this.name} as not enough blocks to prune")
-            return
-        }
-
-        // Get the records to prune
-        val records = archiveService.findRecordsToPrune(prunerEndBlock)
-        if (records.isEmpty()) {
-            logger.info("No records to prune for ${this.name}")
-            return
-        }
-
-        // Prune the records in chunks to avoid memory issues
-        logger.info(
-            "Pruning ${records.size} records for ${this.name} (in chunks of $prunerRemovalChunkSize)",
-        )
-        records.chunked(prunerRemovalChunkSize).forEach { chunk -> archiveService.removeAll(chunk) }
+        prunerService.runPruner(this.name, this.currentBlockNumber, this.status)
     }
 }
