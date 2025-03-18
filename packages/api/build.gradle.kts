e114453d8342e677093ea14ffe7d9f8f58d15831
Roisin Dowling <55144940+roisindowling@users.noreply.github.com>
2025-03-18 23:36:29 +0700

    Replace Tomcat with Jetty (#535)

--- packages/api/build.gradle.kts
+++ packages/api/build.gradle.kts
@@ -5,9 +5,11 @@ plugins {
 }
 
 dependencies {
-
     implementation(project(":packages:common"))
-    implementation("org.springframework.boot:spring-boot-starter-web:3.4.2")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.4.2") {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-jetty:3.4.2")
     implementation("org.springframework.boot:spring-boot-starter-validation:3.4.2")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
