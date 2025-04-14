4666f2017ab506fedc5aef4f166a250bcdffad9c
vechain-ci <vechain.ci@vechain.org>
2025-04-14 12:23:03 +0100

    cicd: fix: Datadog integration taken from TF control

--- terraform/vpc/datadog.tf
+++ terraform/vpc/datadog.tf
@@ -1,1 +1,6 @@
 # module superceded, datadog integration step taken out of TF control
+# module.datadog_integration_aws.data.aws_caller_identity.current id "905964754131"
+# module.datadog_integration_aws.data.aws_iam_policy_document.datadog_aws_integration id "423634147"
+# module.datadog_integration_aws.data.aws_secretsmanager_secret_version.datadog_api_keys id "arn:aws:secretsmanager:eu-west-1:905964754131:secret:prod/datadog_keys-NFBO6v|AWSCURRENT"
+# module.datadog_integration_aws.aws_iam_policy.datadog_aws_integration id "arn:aws:iam::905964754131:policy/DatadogAWSIntegrationPolicy"
+# module.datadog_integration_aws.datadog_dashboard.project_dashboard id "jxv-pdv-wij"
