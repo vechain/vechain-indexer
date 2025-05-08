4d0d3f9005189327347a35853d237057d6776775
Hamza Abdullahi <hamza.abdullahi@vechain.org>
2025-05-08 15:36:41 +0100

    Testing: removing hardcoded value for version

--- packages/indexer/src/main/kotlin/org/vechain/indexer/service/IndexerVersionService.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/service/IndexerVersionService.kt
@@ -29,7 +29,7 @@ open class IndexerVersionService(
                 logger.info("No version document found for $collectionName. No action taken.")
                 updateIndexerVersion(
                     collectionName,
-                    1,
+                    newVersion,
                 )
                 return false
             }
