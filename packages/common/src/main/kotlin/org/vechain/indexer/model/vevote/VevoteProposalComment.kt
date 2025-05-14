6866210127869a1a9e032c9b6f7a9062f45750a0
roisindowling <roisin.dowling@vechain.org>
2025-05-14 13:54:34 +0100

    Clean up

--- packages/common/src/main/kotlin/org/vechain/indexer/model/vevote/VevoteProposalComment.kt
+++ packages/common/src/main/kotlin/org/vechain/indexer/model/vevote/VevoteProposalComment.kt
@@ -1,44 +1,49 @@
-package org.vechain.indexer.model
+package org.vechain.indexer.model.vevote
 
 import com.fasterxml.jackson.annotation.JsonIgnore
-import java.math.BigDecimal
+import java.math.BigInteger
+import org.apache.commons.codec.digest.DigestUtils
 import org.springframework.boot.context.properties.bind.ConstructorBinding
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import org.vechain.indexer.model.IndexedDocument
 import org.vechain.indexer.thor.model.Block
 
-@Document(collection = "vevote_proposal_results")
-data class VeVoteProposalResults
+@Document(collection = "vevote_proposal_comments")
+data class VevoteProposalComment
 @ConstructorBinding
 constructor(
     @JsonIgnore @Id val id: String,
     @JsonIgnore override val blockId: String,
     override val blockNumber: Long,
     override val blockTimestamp: Long,
+    val voter: String,
     val proposalId: String,
-    val choice: Int,
-    val totalWeight: BigDecimal,
-    val totalVoters: Int,
+    val choices: List<Int>,
+    val weight: BigInteger,
+    val reason: String,
 ) : IndexedDocument {
     constructor(
         block: Block,
+        voter: String,
         proposalId: String,
-        choice: Int,
-        totalWeight: BigDecimal,
-        totalVoters: Int,
+        choices: List<Int>,
+        weight: BigInteger,
+        reason: String,
     ) : this(
-        id = generateVoteAggregateId(proposalId, choice),
+        id = generateId(proposalId, reason),
         blockId = block.id,
         blockNumber = block.number,
         blockTimestamp = block.timestamp,
+        voter = voter,
         proposalId = proposalId,
-        choice = choice,
-        totalWeight = totalWeight,
-        totalVoters = totalVoters,
+        choices = choices,
+        weight = weight,
+        reason = reason,
     )
 }
 
-fun generateVoteAggregateId(
+fun generateId(
     proposalId: String,
-    choice: Int,
-): String = "$proposalId-$choice"
+    reason: String,
+): String = DigestUtils.sha1Hex("$proposalId-${reason.trim().lowercase()}")
