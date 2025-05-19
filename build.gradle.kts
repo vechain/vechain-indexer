28c76d9c69f1aa33b0f4467f33edebc198e255e1
Daithi Hearn <daithi.hearn@vechain.org>
2025-05-19 11:24:29 +0200

    Upgrading to version 4.1.1 of indexer-core

--- build.gradle.kts
+++ build.gradle.kts
@@ -235,7 +235,7 @@ allprojects {
         implementation("commons-codec:commons-codec:1.15")
 
         // Core indexer dependency
-        implementation("org.vechain:indexer-core:4.0.0")
+        implementation("org.vechain:indexer-core:4.1.1")
 
         // Test dependencies
         testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.5")
