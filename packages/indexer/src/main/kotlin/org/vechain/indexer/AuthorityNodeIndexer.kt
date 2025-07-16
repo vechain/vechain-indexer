95605164d11683e838279e6c969e1eafd44054e1
roisindowling <roisin.dowling@vechain.org>
2025-07-16 08:32:50 +0100

    Update Indexer

--- packages/indexer/src/main/kotlin/org/vechain/indexer/AuthorityNodeIndexer.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/AuthorityNodeIndexer.kt
@@ -6,38 +6,55 @@ import org.springframework.stereotype.Component
 import org.vechain.indexer.event.AbiManager
 import org.vechain.indexer.event.model.generic.FilterCriteria
 import org.vechain.indexer.repository.AuthorityNodeRepository
-import org.vechain.indexer.service.AuthorityNodeEventService
+import org.vechain.indexer.service.AuthorityNodesService
+import org.vechain.indexer.service.ThorService
 import org.vechain.indexer.thor.client.ThorClient
 import org.vechain.indexer.thor.enums.LogType
+import org.vechain.indexer.thor.model.BlockIdentifier
 import org.vechain.indexer.thor.model.EventLog
 import org.vechain.indexer.thor.model.TransferLog
 
 @Profile("authority-nodes")
 @Component
-open class AuthorityNodeEndorserIndexer(
+open class AuthorityNodeIndexer(
     private val authorityNodeRepository: AuthorityNodeRepository,
-    private val authorityNodeEventService: AuthorityNodeEventService,
+    private val authorityNodeService: AuthorityNodesService,
+    private val thorService: ThorService,
     thorClient: ThorClient,
     abiManager: AbiManager,
-    @Value("\${indexer.startBlock.authority_nodes}") startBlock: Long,
     @Value("\${indexer.syncLogInterval.authority_nodes}") private val syncLogInterval: Long,
     @Value("\${indexer.syncBlockBatchSize.authority_nodes}") private val syncBlockBatchSize: Long,
     @Value("\${veworld.contract.authority_node.address}") private val contractAddress: String,
 ) :
     BaseLogIndexer(
         repository = authorityNodeRepository,
-        startBlock = startBlock,
         thorClient = thorClient,
         syncLogInterval = syncLogInterval,
         blockBatchSize = syncBlockBatchSize,
         logsType = setOf(LogType.EVENT),
         abiManager = abiManager,
     ) {
-    var initialSyncChecked = false
+    private var hasSynced = false
+
+    override fun getLastSyncedBlock(): BlockIdentifier? {
+        if (!hasSynced && authorityNodeRepository.count() == 0L) {
+            val bestBlock = thorService.getBestBlock()
+            return BlockIdentifier(id = bestBlock.id, number = bestBlock.number)
+        }
+        return super.getLastSyncedBlock()
+    }
 
     override fun processLogs(events: List<EventLog>, transfers: List<TransferLog>) {
+        if (!hasSynced && authorityNodeRepository.count() == 0L) {
+            logger.info("No Authority Nodes found – syncing after collection setup...")
+            authorityNodeService.syncEndorsersForAllNodes()
+            logger.info("Initial Authority Node sync complete.")
+
+            hasSynced = true
+        }
+
         val candidateEvents =
-            processAllEvents(
+            processBlockGenericEvents(
                 events,
                 transfers,
                 FilterCriteria(
@@ -45,19 +62,8 @@ open class AuthorityNodeEndorserIndexer(
                     eventNames = listOf("Candidate"),
                 ),
             )
-        // After the first full-sync, check any newly-added AMNs
-        authorityNodeEventService.processCandidateEvents(
-            candidateEvents,
-            this.status == Status.FULLY_SYNCED,
-        )
 
-        // check all AMNs in DB when first fully synced
-        if (this.status == Status.FULLY_SYNCED && !initialSyncChecked) {
-            logger.info("Indexer fully synced - checking all node endorsers...")
-            authorityNodeEventService.syncEndorsersForAllNodes()
-            logger.info("Checked endorsers for all Authority Nodes")
-            initialSyncChecked = true
-        }
+        authorityNodeService.processCandidateEvents(candidateEvents)
     }
 
     override fun rollback(blockNumber: Long) {
