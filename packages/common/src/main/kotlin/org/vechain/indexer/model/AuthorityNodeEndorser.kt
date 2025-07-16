ddd95442e5c94eb13bdd3b1bb86e4c320d86398e
roisindowling <roisin.dowling@vechain.org>
2025-07-16 08:38:49 +0100

    Hide block number form response body

--- packages/common/src/main/kotlin/org/vechain/indexer/model/AuthorityNodeEndorser.kt
+++ packages/common/src/main/kotlin/org/vechain/indexer/model/AuthorityNodeEndorser.kt
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document
 @Document(collection = "authority_nodes")
 data class AuthorityNodeEndorser(
     @Id val nodeMaster: String,
-    override val blockNumber: Long,
+    @JsonIgnore override val blockNumber: Long,
     @JsonIgnore override val blockId: String,
     @JsonIgnore override val blockTimestamp: Long,
     val endorser: String? = null,
