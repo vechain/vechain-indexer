a1dc85ba7567078504989d6ea16f4c4ca9a2ef04
Roisin Dowling <55144940+roisindowling@users.noreply.github.com>
2025-07-25 08:48:00 +0100

    Split VeVote controller into two separate ones (#666)

--- packages/api/src/main/kotlin/org/vechain/indexer/controller/VeVoteCommentsController.kt
+++ packages/api/src/main/kotlin/org/vechain/indexer/controller/VeVoteCommentsController.kt
@@ -1,43 +1,31 @@
 package org.vechain.indexer.controller
 
-import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.*
 import io.swagger.v3.oas.annotations.enums.ParameterIn
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.context.annotation.Profile
-import org.springframework.data.domain.Slice
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.vechain.indexer.constants.VEVOTE_PATH
 import org.vechain.indexer.docs.PaginationParameters
-import org.vechain.indexer.exception.BadRequestException
 import org.vechain.indexer.model.Address
 import org.vechain.indexer.model.rest.PaginatedResponse
 import org.vechain.indexer.model.rest.paginatedResponse
-import org.vechain.indexer.model.vevote.Support
-import org.vechain.indexer.model.vevote.VeVoteProposalComment
-import org.vechain.indexer.model.vevote.VeVoteProposalResults
-import org.vechain.indexer.service.VeVoteResultsService
+import org.vechain.indexer.model.vevote.*
 import org.vechain.indexer.service.VeVoteService
 import org.vechain.indexer.utils.PaginationUtils.toPageable
 import org.vechain.indexer.validation.ValidAddress
 import org.vechain.indexer.validation.ValidPageSize
 
-@Profile("vevote-comments", "vevote-results")
-@Tag(name = "VeVote", description = "Indexer API for querying VeVote proposal information.")
+@Profile("vevote-comments")
+@Tag(name = "VeVote", description = "Indexer API for VeVote.")
 @Validated
 @RestController
 @RequestMapping(VEVOTE_PATH)
-open class VeVoteController(
-    private val vevoteService: VeVoteService,
-    private val resultService: VeVoteResultsService,
-) {
+open class VeVoteCommentsController(private val vevoteService: VeVoteService) {
     @GetMapping("proposals/comments")
     @Operation(summary = "Get comments for a proposal.")
     @ApiResponses(
@@ -82,7 +70,6 @@ open class VeVoteController(
     ): PaginatedResponse<VeVoteProposalComment> {
         val pageable = toPageable(page, size, direction, VeVoteProposalComment::blockNumber.name)
 
-        // Either a proposalId or voter or support must be provided
         val result =
             when {
                 proposalId != null && voter != null && support != null ->
@@ -100,58 +87,7 @@ open class VeVoteController(
                 voter != null && support != null ->
                     vevoteService.getCommentsByVoterAndSupport(voter.value, support, pageable)
                 voter != null -> vevoteService.getCommentsByVoter(voter.value, pageable)
-                else -> // only support is not null
-                vevoteService.getCommentsBySupport(support!!, pageable)
-            }
-        return paginatedResponse(result)
-    }
-
-    @GetMapping("proposal/results")
-    @Operation(summary = "Returns a list of results on vote weight per support")
-    @ApiResponses(
-        value =
-            [
-                ApiResponse(responseCode = "200", description = "Success"),
-                ApiResponse(
-                    responseCode = "400",
-                    description = "A valid proposalId or voter address must be provided",
-                ),
-            ]
-    )
-    @Parameter(
-        `in` = ParameterIn.QUERY,
-        name = "proposalId",
-        description = "Proposal ID to filter by.",
-        required = false,
-        schema = Schema(type = "string"),
-    )
-    @Parameter(
-        `in` = ParameterIn.QUERY,
-        name = "support",
-        schema = Schema(implementation = Support::class),
-        description = "Filter by support: AGAINST, FOR, or ABSTAIN.",
-        required = false,
-    )
-    @PaginationParameters
-    open fun getResults(
-        @RequestParam(required = false) proposalId: String?,
-        @RequestParam(required = false) support: Support?,
-        @RequestParam(required = false) page: Int?,
-        @ValidPageSize @RequestParam(required = false) size: Int?,
-        @RequestParam(required = false) direction: String?,
-    ): PaginatedResponse<VeVoteProposalResults> {
-        if (proposalId == null && support == null) {
-            throw BadRequestException("Either a proposalId or support must be provided")
-        }
-
-        val pageable = toPageable(page, size, direction, VeVoteProposalResults::blockNumber.name)
-
-        val result: Slice<VeVoteProposalResults> =
-            when {
-                proposalId != null && support != null ->
-                    resultService.getResultsByProposalIdAndSupport(proposalId, support, pageable)
-                proposalId != null -> resultService.getResultsByProposalId(proposalId, pageable)
-                else -> resultService.getResultsBySupport(support!!, pageable)
+                else -> vevoteService.getCommentsBySupport(support!!, pageable)
             }
 
         return paginatedResponse(result)
