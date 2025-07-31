0fbbb7c7d1399189a8102fa5dc89d1638db18914
roisindowling <roisin.dowling@vechain.org>
2025-07-31 17:44:25 +0100

    add delegationSC address to VTHClaimed Indexer

--- packages/indexer/src/main/kotlin/org/vechain/indexer/stargate/VthoClaimedByBlockConfig.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/stargate/VthoClaimedByBlockConfig.kt
@@ -22,6 +22,8 @@ open class VthoClaimedByBlockConfig {
         @Value("\${indexer.syncLogInterval.stargate}") logInterval: Long,
         @Value("\${business-event.substitutions.STARGATE_NFT_CONTRACT}")
         stargateNftContract: String,
+        @Value("\${business-event.substitutions.STARGATE_DELEGATION_CONTRACT}")
+        stargateDelegationContract: String,
         bEProperties: BusinessEventProperties,
     ): Indexer =
         IndexerFactory()
@@ -35,7 +37,9 @@ open class VthoClaimedByBlockConfig {
             .businessEventNames(
                 listOf("STARGATE_CLAIM_REWARDS_BASE", "STARGATE_CLAIM_REWARDS_DELEGATE")
             )
-            .businessEventContracts(listOf(stargateNftContract, VTHO_CONTRACT))
+            .businessEventContracts(
+                listOf(stargateNftContract, stargateDelegationContract, VTHO_CONTRACT)
+            )
             .businessEventSubstitutionParams(bEProperties.substitutions)
             .build()
 }
