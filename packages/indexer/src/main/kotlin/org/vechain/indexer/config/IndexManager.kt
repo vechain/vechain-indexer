981dcffcb409c083e5b30286fb6a176e7e2aa3a5
Daithi Hearn <daithi.hearn@vechain.org>
2025-08-06 15:10:02 +0200

    Moving coroutine scope bean into it's own config

--- packages/indexer/src/main/kotlin/org/vechain/indexer/config/IndexManager.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/config/IndexManager.kt
@@ -1,14 +1,11 @@
 package org.vechain.indexer.config
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
 import org.slf4j.LoggerFactory
 import org.springframework.boot.ExitCodeGenerator
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.ApplicationContext
-import org.springframework.context.annotation.Bean
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 import org.vechain.indexer.Indexer
@@ -17,13 +14,12 @@ import org.vechain.indexer.Indexer
 class IndexManager(
     private val indexers: List<Indexer>,
     private val applicationContext: ApplicationContext,
+    private val appCoroutineScope: CoroutineScope,
 ) {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
-    @Bean fun appCoroutineScope(): CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
-
     @EventListener(ApplicationReadyEvent::class)
-    fun start(appCoroutineScope: CoroutineScope) {
+    fun start() {
         logger.info("Starting ${indexers.size} indexers")
 
         indexers.forEach { indexer ->
