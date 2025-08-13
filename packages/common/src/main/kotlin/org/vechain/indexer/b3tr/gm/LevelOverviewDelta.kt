da6c7ff22e7767e43b721c20ba38d4565d5902f0
roisindowling <roisin.dowling@vechain.org>
2025-08-13 16:27:19 +0100

    remove level overview indexer

--- packages/common/src/main/kotlin/org/vechain/indexer/b3tr/gm/LevelOverviewDelta.kt
+++
@@ -1,9 +0,0 @@
-package org.vechain.indexer.b3tr.gm
-
-import java.math.BigInteger
-
-data class LevelOverviewDelta(
-    var nftsDiff: Long = 0,
-    var b3trDelta: BigInteger = BigInteger.ZERO,
-    var nodesAttached: Long = 0,
-)
