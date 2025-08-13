5986486f027c10aa22693b40723d407cd6da0e0a
roisindowling <roisin.dowling@vechain.org>
2025-08-13 17:00:05 +0100

    Update B3TR GM Models

--- packages/common/src/main/kotlin/org/vechain/indexer/b3tr/gm/repository/GmNftRepository.kt
+++ packages/common/src/main/kotlin/org/vechain/indexer/b3tr/gm/repository/GmNftRepository.kt
@@ -1,12 +1,25 @@
 package org.vechain.indexer.b3tr.gm.repository
 
 import org.springframework.context.annotation.Profile
+import org.springframework.data.mongodb.repository.Aggregation
 import org.springframework.stereotype.Repository
 import org.vechain.indexer.BasePagingAndSortingIndexedRepository
+import org.vechain.indexer.b3tr.gm.GMLevelOverview
+import org.vechain.indexer.b3tr.gm.GmLevelName
 import org.vechain.indexer.b3tr.gm.GmNft
 
 @Profile("b3tr", "gm-nft")
 @Repository
 interface GmNftRepository : BasePagingAndSortingIndexedRepository<GmNft, String> {
-    fun findAllByAttachedNodeIdNotNull(): List<GmNft>
+    fun countByLevel(level: GmLevelName): Long
+
+    @Aggregation(
+        pipeline =
+            [
+                "{ \$group: { _id: '\$level', totalNFTs: { \$sum: 1 } } }",
+                "{ \$project: { _id: 0, level: '\$_id', totalNFTs: 1 } }",
+                "{ \$sort: { totalNFTs: -1 } }",
+            ]
+    )
+    fun levelCounts(): List<GMLevelOverview>
 }
