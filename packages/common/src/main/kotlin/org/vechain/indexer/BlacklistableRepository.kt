6d1c66cfa3af963eb907b161941709d435a728db
Daithi Hearn <daithi.hearn@vechain.org>
2025-08-13 17:27:01 +0100

    Improving the performance of the blacklist queries by removing the lookup

--- packages/common/src/main/kotlin/org/vechain/indexer/BlacklistableRepository.kt
+++ packages/common/src/main/kotlin/org/vechain/indexer/BlacklistableRepository.kt
@@ -6,34 +6,30 @@ import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.aggregation.Aggregation
 import org.springframework.data.mongodb.core.query.Criteria
 import org.vechain.indexer.nft.IndexedNft
+import org.vechain.indexer.nft.NftBlacklist
+import org.vechain.indexer.nft.NftBlacklistRepository
 import org.vechain.indexer.utils.SliceBuilder
 
 abstract class BlacklistableRepository<T : Any>(
     private val mongoTemplate: MongoTemplate,
+    private val repo: NftBlacklistRepository,
     private val entityClass: Class<T>,
 ) {
     open fun findNotBlacklisted(criteria: Criteria, pageable: Pageable): Slice<T> {
-        val matchOperation = Aggregation.match(criteria)
-        val lookupBlacklistOperation =
-            Aggregation.lookup(
-                "nft_blacklist",
-                IndexedNft::contractAddress.name,
-                "_id",
-                "blacklistInfo",
-            )
-        val matchBlacklistOperation =
+
+        // Get blacklisted NFTs
+        val blacklisted: List<NftBlacklist> = repo.findByIsBlacklisted(true)
+
+        val matchOperation =
             Aggregation.match(
-                Criteria()
-                    .orOperator(
-                        Criteria.where("blacklistInfo.isBlacklisted").ne(true),
-                        Criteria.where("blacklistInfo").exists(false),
-                    )
+                criteria
+                    .and(IndexedNft::contractAddress.name)
+                    .nin(blacklisted.map { it.contractAddress })
             )
+
         val aggregation =
             Aggregation.newAggregation(
                 matchOperation,
-                lookupBlacklistOperation,
-                matchBlacklistOperation,
                 Aggregation.sort(pageable.sort),
                 Aggregation.skip((pageable.pageNumber * pageable.pageSize).toLong()),
                 Aggregation.limit(pageable.pageSize.toLong() + 1),
