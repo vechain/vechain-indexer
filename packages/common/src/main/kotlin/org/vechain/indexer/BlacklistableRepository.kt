e1f5a9e5ecebb9d4ed586ff37978899029cd6da6
Daithi Hearn <37153003+daithihearn@users.noreply.github.com>
2025-08-14 12:57:23 +0200

    Improving the performance of the blacklist queries (#695)

--- packages/common/src/main/kotlin/org/vechain/indexer/BlacklistableRepository.kt
+++ packages/common/src/main/kotlin/org/vechain/indexer/BlacklistableRepository.kt
@@ -6,34 +6,39 @@ import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.aggregation.Aggregation
 import org.springframework.data.mongodb.core.query.Criteria
 import org.vechain.indexer.nft.IndexedNft
+import org.vechain.indexer.nft.NftBlacklist
+import org.vechain.indexer.nft.NftBlacklistRepository
 import org.vechain.indexer.utils.SliceBuilder
 
 abstract class BlacklistableRepository<T : Any>(
     private val mongoTemplate: MongoTemplate,
+    private val repo: NftBlacklistRepository,
     private val entityClass: Class<T>,
 ) {
     open fun findNotBlacklisted(criteria: Criteria, pageable: Pageable): Slice<T> {
-        val matchOperation = Aggregation.match(criteria)
-        val lookupBlacklistOperation =
-            Aggregation.lookup(
-                "nft_blacklist",
-                IndexedNft::contractAddress.name,
-                "_id",
-                "blacklistInfo",
-            )
-        val matchBlacklistOperation =
-            Aggregation.match(
+
+        // Get blacklisted NFTs
+        val blacklisted: List<NftBlacklist> = repo.findByIsBlacklisted(true)
+
+        // Build blacklist predicate only when there are blacklisted addresses
+        val blacklistedAddresses = blacklisted.map { it.contractAddress }
+        val combinedCriteria =
+            if (blacklistedAddresses.isEmpty()) {
+                // Nothing to exclude
+                criteria
+            } else {
+                // Avoid "second 'contractAddress' expression" by wrapping with $and
                 Criteria()
-                    .orOperator(
-                        Criteria.where("blacklistInfo.isBlacklisted").ne(true),
-                        Criteria.where("blacklistInfo").exists(false),
+                    .andOperator(
+                        criteria,
+                        Criteria.where(IndexedNft::contractAddress.name).nin(blacklistedAddresses),
                     )
-            )
+            }
+        val matchOperation = Aggregation.match(combinedCriteria)
+
         val aggregation =
             Aggregation.newAggregation(
                 matchOperation,
-                lookupBlacklistOperation,
-                matchBlacklistOperation,
                 Aggregation.sort(pageable.sort),
                 Aggregation.skip((pageable.pageNumber * pageable.pageSize).toLong()),
                 Aggregation.limit(pageable.pageSize.toLong() + 1),
--- packages/common/src/main/kotlin/org/vechain/indexer/history/HistoryRepositoryImpl.kt
+++ packages/common/src/main/kotlin/org/vechain/indexer/history/HistoryRepositoryImpl.kt
@@ -4,8 +4,13 @@ import org.springframework.context.annotation.Profile
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.stereotype.Component
 import org.vechain.indexer.BlacklistableRepository
+import org.vechain.indexer.nft.NftBlacklistRepository
 
 @Profile("history")
 @Component
-open class HistoryRepositoryImpl(mongoTemplate: MongoTemplate) :
-    BlacklistableRepository<IndexedHistoryEvent>(mongoTemplate, IndexedHistoryEvent::class.java)
+open class HistoryRepositoryImpl(mongoTemplate: MongoTemplate, repo: NftBlacklistRepository) :
+    BlacklistableRepository<IndexedHistoryEvent>(
+        mongoTemplate,
+        repo,
+        IndexedHistoryEvent::class.java,
+    )
