ff50fd8ce53ffac3e5c7f9d814aaa5e879f910e6
roisindowling <roisin.dowling@vechain.org>
2025-08-28 10:39:36 +0100

    Add archive collection for vevote results

--- packages/common/src/main/kotlin/org/vechain/indexer/vevote/VeVoteProposalResults.kt
+++ packages/common/src/main/kotlin/org/vechain/indexer/vevote/VeVoteProposalResults.kt
@@ -1,11 +1,14 @@
 package org.vechain.indexer.vevote
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonView
 import java.math.BigDecimal
 import org.springframework.boot.context.properties.bind.ConstructorBinding
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
-import org.vechain.indexer.IndexedDocument
+import org.vechain.indexer.VersionedDocument
+import org.vechain.indexer.archive.Archive
+import org.vechain.indexer.thor.model.Views
 
 @Document(collection = "vevote_proposal_results")
 data class VeVoteProposalResults
@@ -15,8 +18,18 @@ constructor(
     @JsonIgnore override val blockId: String,
     override val blockNumber: Long,
     override val blockTimestamp: Long,
+    @JsonView(Views.Internal::class) override val version: Int,
     val proposalId: String,
     val support: Support,
     val totalWeight: BigDecimal,
     val totalVoters: Int,
-) : IndexedDocument
+) : VersionedDocument {
+    @JsonIgnore override fun getDocumentId(): String = id
+}
+
+@Document("vevote_proposal_results_archives")
+@JsonView(Views.Public::class)
+data class VeVoteProposalResultsArchive
+@ConstructorBinding
+constructor(@Id override val id: String, override val data: VeVoteProposalResults) :
+    Archive<VeVoteProposalResults>
