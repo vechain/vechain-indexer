b75132827d900be793eb08f2113eb226ae6d1492
roisindowling <roisin.dowling@vechain.org>
2025-09-12 17:11:01 -0400

    Add controller

--- packages/common/src/main/kotlin/org/vechain/indexer/validator/ValidatorRepository.kt
+++ packages/common/src/main/kotlin/org/vechain/indexer/validator/ValidatorRepository.kt
@@ -1,9 +1,17 @@
 package org.vechain.indexer.validator
 
 import org.springframework.context.annotation.Profile
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
 import org.springframework.stereotype.Repository
 import org.vechain.indexer.BasePagingAndSortingIndexedRepository
 
 @Profile("validator")
 @Repository
-interface ValidatorRepository : BasePagingAndSortingIndexedRepository<Validator, String>
+interface ValidatorRepository : BasePagingAndSortingIndexedRepository<Validator, String> {
+    fun findByDelegationIdListIn(delegationIds: List<String>): List<Validator>
+
+    fun findByStatus(status: Status, pageable: Pageable): Slice<Validator>
+
+    fun findByEndorser(endorser: String, pageable: Pageable): Slice<Validator>
+}
