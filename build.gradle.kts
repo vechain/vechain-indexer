7da453927f1059a30d63665f35d96ba7f9ad0d5c
Daithi Hearn <daithi.hearn@vechain.org>
2025-09-15 13:15:02 +0200

    Upgrading to indexer-core 5.0.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -223,7 +223,7 @@ allprojects {
         implementation("commons-codec:commons-codec:1.15")
 
         // Core indexer dependency
-        implementation("org.vechain:indexer-core:5.0.0")
+        implementation("org.vechain:indexer-core:5.0.1-SNAPSHOT")
 
         // Test dependencies
         testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.5")
