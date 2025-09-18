28dd14c7721453c117dda708d51cbdb24e7259f3
Daithi Hearn <daithi.hearn@vechain.org>
2025-09-18 21:27:50 +0200

    trying new query for pruner

--- packages/indexer/src/main/kotlin/org/vechain/indexer/archive/ArchiveService.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/archive/ArchiveService.kt
@@ -2,8 +2,13 @@ package org.vechain.indexer.archive
 
 import org.bson.Document
 import org.slf4j.LoggerFactory
+import org.springframework.data.domain.Sort
 import org.springframework.data.mongodb.core.BulkOperations
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.aggregation.Aggregation
+import org.springframework.data.mongodb.core.aggregation.AggregationOperation
+import org.springframework.data.mongodb.core.aggregation.AggregationOperationContext
+import org.springframework.data.mongodb.core.aggregation.AggregationOptions
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.transaction.annotation.Transactional
@@ -11,6 +16,12 @@ import org.vechain.indexer.IndexedDocument
 import org.vechain.indexer.VersionedDocument
 import org.vechain.indexer.utils.IdUtils
 import org.vechain.indexer.utils.JsonUtils
+
+/** Helper to inject a raw aggregation stage (here: $setWindowFields). */
+class RawStage(private val stage: Document) : AggregationOperation {
+    @Deprecated("Deprecated in Java")
+    override fun toDocument(context: AggregationOperationContext): Document = stage
+}
 
 open class ArchiveService<T : VersionedDocument, S : Archive<T>>(
     private val mongoTemplate: MongoTemplate,
@@ -112,59 +123,43 @@ open class ArchiveService<T : VersionedDocument, S : Archive<T>>(
     open fun findRecordsToPrune(endBlock: Long): List<String> {
         logger.info("Finding records to prune for {}", clazz.simpleName)
 
-        // Construct the aggregation pipeline stages
-        val matchStage =
-            Document("\$match", Document("data.blockNumber", Document("\$lt", endBlock)))
-
-        val removeRedundantFieldsStage =
-            Document(
-                "\$project",
-                Document().append("recordId", "\$data._id").append("version", "\$data.version"),
-            )
-
-        val groupStage =
-            Document(
-                "\$group",
-                Document("_id", "\$recordId")
-                    .append("maxVersion", Document("\$max", "\$version"))
-                    .append("allDocs", Document("\$push", "\$\$ROOT")),
-            )
-
-        val unwindStage = Document("\$unwind", "\$allDocs")
-
-        val matchExprStage =
-            Document(
-                "\$match",
+        // 1) Rank by recordId/version (desc)
+        val setWindowFields =
+            RawStage(
                 Document(
-                    "\$expr",
-                    Document(
-                        "\$lt",
-                        listOf(
-                            "\$allDocs.version",
-                            Document("\$subtract", listOf("\$maxVersion", 4)),
-                        ),
-                    ),
-                ),
+                    "\$setWindowFields",
+                    Document()
+                        .append("partitionBy", "\$data._id")
+                        .append("sortBy", Document("data.version", -1))
+                        .append("output", Document("rn", Document("\$documentNumber", Document()))),
+                )
             )
 
-        val projectStage = Document("\$project", Document("_id", "\$allDocs._id"))
-
-        // Combine all stages into a pipeline
+        // 2) Build pipeline
         val pipeline =
-            listOf(
-                matchStage,
-                removeRedundantFieldsStage,
-                groupStage,
-                unwindStage,
-                matchExprStage,
-                projectStage,
-            )
+            Aggregation.newAggregation(
+                    // NOTE: rank globally first to truly "keep latest N" overall,
+                    // then apply the endBlock cutoff (so you don't miscount if newest are >=
+                    // endBlock)
+                    Aggregation.sort(
+                        Sort.by(Sort.Order.asc("data._id"), Sort.Order.desc("data.version"))
+                    ),
+                    setWindowFields,
+                    Aggregation.match(
+                        Criteria.where("rn").gt(1).and("data.blockNumber").lt(endBlock)
+                    ),
+                    Aggregation.project("_id"),
+                    Aggregation.limit(50_000),
+                )
+                .withOptions(AggregationOptions.builder().allowDiskUse(true).build())
 
         // Execute the aggregation
         return mongoTemplate
-            .getCollection(mongoTemplate.getCollectionName(archiveClazz))
-            .aggregate(pipeline)
-            .allowDiskUse(true)
+            .aggregate(
+                pipeline,
+                mongoTemplate.getCollectionName(archiveClazz),
+                Document::class.java,
+            )
             .map { it.getString("_id") }
             .toList()
     }
