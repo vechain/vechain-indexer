217da165e37bb1b24b9dd02d27d074b6d1393a54
roisindowling <roisin.dowling@vechain.org>
2025-09-23 10:31:52 -0400

    Add support for stargate boost BE

--- packages/indexer/src/main/kotlin/org/vechain/indexer/utils/EventUtils.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/utils/EventUtils.kt
@@ -51,6 +51,7 @@ object EventUtils {
             "STARGATE_DELEGATE_LEGACY" -> HistoryEventName.STARGATE_DELEGATE_LEGACY
             "STARGATE_DELEGATION_EXIT_REQUEST" -> HistoryEventName.STARGATE_DELEGATE_EXIT_REQUEST
             "STARGATE_CLAIM_REWARDS" -> HistoryEventName.STARGATE_CLAIM_REWARDS
+            "STARGATE_BOOST" -> HistoryEventName.STARGATE_BOOST
             else -> null // Other events will not be labeled
         }
 
