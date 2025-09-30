609fd3cfa4b116558e5565822bc54e7454fea66c
Dougal Rea <73436291+dougalrea@users.noreply.github.com>
2025-09-30 12:39:47 +0800

    devnet infra (#761)
    Co-authored-by: rishi <rishi.pal@vechain.org>

---
+++ terraform/devnet/outputs.tf
@@ -0,0 +1,14 @@
+output "cluster_name" {
+ description = "The name of the ECS cluster"
+ value = length(module.ecs-cluster) > 0 ? module.ecs-cluster.name : ""
+ }
+
+output "load_balancer_domain_devnet" {
+  description = "The domain name of the mainnet load balancer"
+  value       = length(module.ecs-lb-service-api) > 0 ? module.ecs-lb-service-api["dev"].alb_dns_name : ""
+}
+
+output "devnet_domain" {
+  description = "The domain name of the devnet environment"
+  value = aws_route53_record.devnet.fqdn
+}
