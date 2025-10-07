1299286ab8dfb6d21e669c062d690aede579ce49
Daithi Hearn <daithi.hearn@vechain.org>
2025-10-07 19:51:19 +0200

    Updating core version

--- build.gradle.kts
+++ build.gradle.kts
@@ -215,7 +215,7 @@ allprojects {
         implementation("commons-codec:commons-codec:1.15")
 
         // Core indexer dependency
-        implementation("org.vechain:indexer-core:6.0.0-SNAPSHOT")
+        implementation("org.vechain:indexer-core:6.0.0")
 
         // Test dependencies
         testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.5")
