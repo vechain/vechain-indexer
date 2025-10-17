b51487e1f32f1890ce94282aa420c266d15a0ae1
Daithi Hearn <daithi.hearn@vechain.org>
2025-10-17 03:00:22 +0200

    Removing redundant tags

--- packages/common/src/main/kotlin/org/vechain/indexer/history/HistoryRepositoryImpl.kt
+++ packages/common/src/main/kotlin/org/vechain/indexer/history/HistoryRepositoryImpl.kt
@@ -1,13 +1,9 @@
 package org.vechain.indexer.history
 
-import org.springframework.context.annotation.Profile
 import org.springframework.data.mongodb.core.MongoTemplate
-import org.springframework.stereotype.Component
 import org.vechain.indexer.BlacklistableRepository
 import org.vechain.indexer.nft.NftBlacklistRepository
 
-@Profile("history")
-@Component
 open class HistoryRepositoryImpl(mongoTemplate: MongoTemplate, repo: NftBlacklistRepository) :
     BlacklistableRepository<IndexedHistoryEvent>(
         mongoTemplate,
