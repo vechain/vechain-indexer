be93dccf5aa011275c5493bb4919dedfddd9e3ec
Daithi Hearn <37153003+daithihearn@users.noreply.github.com>
2025-10-24 15:12:42 +0200

    821 improve the performance of fungible tokens contracts endpoint (#822)

---
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/transfer/FungibleTokenInteractionsCollectionConfig.kt
@@ -0,0 +1,51 @@
+package org.vechain.indexer.transfer
+
+import jakarta.annotation.PostConstruct
+import kotlinx.coroutines.CoroutineScope
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.data.domain.Sort
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.index.Index
+import org.vechain.indexer.IndexerNames
+import org.vechain.indexer.config.mongo.CollectionConfig
+import org.vechain.indexer.version.IndexerVersionService
+
+@Profile("transfers")
+@Configuration
+open class FungibleTokenInteractionsCollectionConfig(
+    mongoTemplate: MongoTemplate,
+    appCoroutineScope: CoroutineScope,
+    private val indexerVersionService: IndexerVersionService,
+) : CollectionConfig(mongoTemplate, appCoroutineScope, IndexedTransferEvent::class.java) {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    @Value("\${indexer.version.fungible-token-interactions}") private val version: Int = 1
+
+    @PostConstruct
+    override fun initCollection() {
+        logger.info("Check collection version for ${modelObj.simpleName}")
+
+        indexerVersionService.checkAndResetCollectionIfVersionChanged(
+            indexerName = IndexerNames.FUNGIBLE_TOKEN_INTERACTIONS,
+            FungibleTokenInteraction::class.java,
+            version,
+        )
+
+        ensureCollection()
+
+        logger.info("Initializing indexes for ${modelObj.simpleName}")
+
+        ensureIndexes(
+            listOf(
+                "blockNumber_-1" to Index().on("blockNumber", Sort.Direction.DESC),
+                "contractAddress_1_walletAddress_1" to
+                    Index()
+                        .on("contractAddress", Sort.Direction.ASC)
+                        .on("walletAddress", Sort.Direction.ASC),
+            )
+        )
+    }
+}
