b48cfe2c2404e276763dbea65ffa9149bd393a2e
Daithi Hearn <daithi.hearn@vechain.org>
2025-10-30 18:42:35 +0100

    formatting

--- packages/common/src/main/kotlin/org/vechain/indexer/b3tr/action/repository/AppRoundActionSummaryRepository.kt
+++ packages/common/src/main/kotlin/org/vechain/indexer/b3tr/action/repository/AppRoundActionSummaryRepository.kt
@@ -33,7 +33,8 @@ interface AppRoundActionSummaryRepository :
 
     @Cacheable(
         value = ["app_round_countByTotalRewardAmountGreaterThanAndAppIdAndRoundId"],
-        key = "#totalRewardAmount.stripTrailingZeros().toPlainString() + '-' + #appId + '-' + #roundId",
+        key =
+            "#totalRewardAmount.stripTrailingZeros().toPlainString() + '-' + #appId + '-' + #roundId",
     )
     fun countByTotalRewardAmountGreaterThanAndAppIdAndRoundId(
         totalRewardAmount: java.math.BigDecimal,
