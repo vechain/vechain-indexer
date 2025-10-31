142ed9c1ea570d78fa59f51df91029d2709f893e
Daithi Hearn <daithi.hearn@vechain.org>
2025-10-31 15:59:27 +0100

    Rolling back regex changes

--- packages/common/src/main/kotlin/org/vechain/indexer/thor/HexUtils.kt
+++ packages/common/src/main/kotlin/org/vechain/indexer/thor/HexUtils.kt
@@ -3,9 +3,9 @@ package org.vechain.indexer.thor
 object HexUtils {
 
     /** Optional prefix 0x */
-    const val REGEX = """^(0x)?[0-9a-fA-F]+$"""
+    const val REGEX = "^(0x)?[0-9a-fA-F]+$"
 
-    const val BLOCK_ID_REGEX = """^(0x)?[0-9a-fA-F]{64}\$"""
+    const val BLOCK_ID_REGEX = "^(0x)?[0-9a-fA-F]{64}\$"
 
     fun compare(hex1: String, hex2: String): Boolean {
         return normalise(hex1) == normalise(hex2)
