3bfdfa9087f8dc7d2a2701d87bbac8e834267b60
Daithi Hearn <daithi.hearn@vechain.org>
2025-11-05 12:11:20 +0100

    Adding missing search fields for account Aligning indexes with prod

--- packages/indexer/src/main/kotlin/org/vechain/indexer/history/HistoryCollectionConfig.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/history/HistoryCollectionConfig.kt
@@ -99,6 +99,17 @@ open class HistoryCollectionConfig(
                         .on("eventName", Sort.Direction.ASC)
                         .on("to", Sort.Direction.ASC)
                         .on("blockTimestamp", Sort.Direction.DESC),
+                "owner_1_blockTimestamp_-1_eventName_1_contractAddress_1" to
+                    Index()
+                        .on("owner", Sort.Direction.ASC)
+                        .on("blockTimestamp", Sort.Direction.DESC)
+                        .on("eventName", Sort.Direction.ASC)
+                        .on("contractAddress", Sort.Direction.ASC),
+                "appId_1_eventName_1_to_1" to
+                    Index()
+                        .on("appId", Sort.Direction.ASC)
+                        .on("eventName", Sort.Direction.ASC)
+                        .on("to", Sort.Direction.ASC),
             )
         )
     }
