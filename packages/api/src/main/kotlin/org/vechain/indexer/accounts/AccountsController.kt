d9ceec94921ce09cbe3c622e14ca136338aa036d
roisindowling <roisin.dowling@vechain.org>
2025-11-10 16:44:40 +0000

    Add accounts controller

---
+++ packages/api/src/main/kotlin/org/vechain/indexer/accounts/AccountsController.kt
@@ -0,0 +1,57 @@
+package org.vechain.indexer.accounts
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.enums.ParameterIn
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.context.annotation.Profile
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.vechain.indexer.constants.ACCOUNTS_PATH
+import org.vechain.indexer.docs.CommonApiResponses
+import org.vechain.indexer.rest.PaginatedResponse
+import org.vechain.indexer.rest.paginatedResponse
+import org.vechain.indexer.utils.PaginationUtils
+import org.vechain.indexer.validation.ValidPageSize
+
+@Profile("accounts")
+@Tag(name = "Accounts", description = "VeChain Thor Accounts Overview")
+@Validated
+@RestController
+@RequestMapping(ACCOUNTS_PATH)
+open class AccountsController(private val accountsService: AccountsService) {
+    @GetMapping("/totals")
+    @Operation(
+        summary = "Retrieve total unique accounts overview",
+        description =
+            """
+            Retrieves historical totals of unique VeChain accounts tracked per time frame (DAY, WEEK, MONTH, YEAR).
+            The "ALL" account aggregates totals across all time frames.
+            
+            If no `timeFrame` is provided, the response defaults to showing the full cumulative totals (ALL).
+        """,
+    )
+    @Parameter(
+        name = "timeFrame",
+        `in` = ParameterIn.QUERY,
+        description = "Time frame to query totals for (DAY, WEEK, MONTH, YEAR, ALL).",
+        required = false,
+        schema = Schema(implementation = TimeFrame::class),
+    )
+    @CommonApiResponses
+    open fun getTotalAccounts(
+        @RequestParam(required = false) timeFrame: TimeFrame?,
+        @RequestParam(required = false) page: Int?,
+        @ValidPageSize @RequestParam(required = false) size: Int?,
+        @RequestParam(required = false) direction: String?,
+    ): PaginatedResponse<Accounts> {
+        val pageable =
+            PaginationUtils.toPageable(page, size, direction, Accounts::blockTimestamp.name)
+        val accounts = accountsService.getTotal(timeFrame, pageable)
+        return paginatedResponse(accounts)
+    }
+}
--- packages/common/src/main/kotlin/org/vechain/indexer/accounts/AccountsRepository.kt
+++ packages/common/src/main/kotlin/org/vechain/indexer/accounts/AccountsRepository.kt
@@ -1,7 +1,11 @@
 package org.vechain.indexer.accounts
 
 import org.springframework.context.annotation.Profile
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
 import org.vechain.indexer.BasePagingAndSortingIndexedRepository
 
 @Profile("accounts")
-interface AccountsRepository : BasePagingAndSortingIndexedRepository<Accounts, String>
+interface AccountsRepository : BasePagingAndSortingIndexedRepository<Accounts, String> {
+    fun findByTimeFrameIn(timeFrames: List<TimeFrame>, pageable: Pageable): Slice<Accounts>
+}
