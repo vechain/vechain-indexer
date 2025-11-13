781bc391a2fa9c9feab9fbee057fe0b7f1dad296
roisindowling <roisin.dowling@vechain.org>
2025-11-13 16:14:28 +0000

    Add missing override

--- packages/common/src/main/kotlin/org/vechain/indexer/stargate/vetStaked/VetStakedByBlockRepository.kt
+++ packages/common/src/main/kotlin/org/vechain/indexer/stargate/vetStaked/VetStakedByBlockRepository.kt
@@ -3,6 +3,7 @@ package org.vechain.indexer.stargate.vetStaked
 import org.springframework.context.annotation.Profile
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Slice
+import org.springframework.data.mongodb.repository.Aggregation
 import org.vechain.indexer.BaseIndexedRepository
 import org.vechain.indexer.accounts.TimeFrame
 import org.vechain.indexer.stargate.timeFrame.TimeFrameRepo
@@ -21,4 +22,14 @@ interface VetStakedByBlockRepository :
     ): List<VetStakedByBlock>
 
     override fun findByBlockTimestampAfter(blockTimestamp: Long): List<VetStakedByBlock>
+
+    @Aggregation(
+        pipeline =
+            [
+                "{ '\$match': { 'blockNumber': { '\$lte': ?0 } } }",
+                "{ '\$sort': { 'blockNumber': -1 } }",
+                "{ '\$limit': 1 }",
+            ]
+    )
+    override fun findLatestBeforeOrAtBlockNumber(blockNumber: Long): VetStakedByBlock?
 }
