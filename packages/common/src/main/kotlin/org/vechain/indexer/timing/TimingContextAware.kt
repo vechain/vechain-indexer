3700aa30445604aff6a5b0c959bf335a35d504c3
Daithi Hearn <daithi.hearn@vechain.org>
2025-12-12 10:16:42 +0100

    Removing TimingAspect.kt as it is made redundant by the prometheus changes

--- packages/common/src/main/kotlin/org/vechain/indexer/timing/TimingContextAware.kt
+++
@@ -1,6 +0,0 @@
-package org.vechain.indexer.timing
-
-/** Implementations can provide additional context that will be appended to timing logs. */
-interface TimingContextAware {
-    fun timingContext(): String
-}
