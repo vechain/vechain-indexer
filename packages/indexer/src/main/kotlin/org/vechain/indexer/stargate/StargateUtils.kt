4d34b53f0f6371da246ae7152744f363e11a81ab
Roisin Dowling <roisinadowling@gmail.com>
2025-12-16 12:31:07 +0000

    Update utils and add tests

--- packages/indexer/src/main/kotlin/org/vechain/indexer/stargate/StargateUtils.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/stargate/StargateUtils.kt
@@ -2,26 +2,18 @@ package org.vechain.indexer.stargate
 
 import org.vechain.indexer.contracts.abi.FunctionDefinition
 import org.vechain.indexer.contracts.abi.FunctionParameter
-import org.vechain.indexer.thor.AddressUtils
 import org.vechain.indexer.thor.model.Clause
 import org.vechain.indexer.utils.ContractUtils
 
 object StargateUtils {
-    val balanceOfFunction =
-        // balanceOf
+    val issuanceFunction =
         FunctionDefinition(
-            name = "balanceOf",
-            inputs = listOf(FunctionParameter("owner", "address", emptyList(), "address")),
-            outputs = listOf(FunctionParameter("balance", "uint256")),
+            name = "issuance",
+            inputs = emptyList(),
+            outputs = listOf(FunctionParameter("issued", "uint256")),
             stateMutability = "view",
         )
 
-    fun buildBalanceOfClause(stargateSC: String): List<Clause> =
-        listOf(
-            ContractUtils.createClause(
-                "0x0000000000000000000000000000456e65726779",
-                balanceOfFunction,
-                AddressUtils.toBigInt(stargateSC),
-            )
-        )
+    fun buildIssuanceClause(stakerSC: String): List<Clause> =
+        listOf(ContractUtils.createClause(stakerSC, issuanceFunction))
 }
