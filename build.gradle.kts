a25e7427f2c2a842e58817a263c3033aefd4a103
Roisin Dowling <roisinadowling@gmail.com>
2025-12-18 17:16:32 +0000

    USE LATEST VERSION OF CORE

--- build.gradle.kts
+++ build.gradle.kts
@@ -223,7 +223,7 @@ allprojects {
         implementation("io.micrometer:micrometer-registry-datadog:1.14.5")
 
         // Core indexer dependency
-        implementation("org.vechain:indexer-core:6.1.0")
+        implementation("org.vechain:indexer-core:6.2.1-SNAPSHOT")
 
         // Test dependencies
         testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.5")
