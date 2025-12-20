fa00e9d523a8a6df0960c2cc5e267ab9f8835cad
rishi <rishi.pal@vechain.org>
2025-12-20 09:57:00 +0000

    quick fix

--- terraform/api/api.tf
+++ terraform/api/api.tf
@@ -1004,7 +1004,7 @@ module "vpc-endpoints" {
 
 module "waf" {
   count  = startswith(local.env.environment, "prod") ? 1 : 0
-  source = "git::git@github.com:/vechain/terraform_infrastructure_modules.git//waf?ref="git::git@github.com:/vechain/terraform_infrastructure_modules.git//waf?ref=waf_indexer_fix"
+  source = "git::git@github.com:/vechain/terraform_infrastructure_modules.git//waf?ref=waf_indexer_fix"
 
   env          = local.env.environment
   project_name = "${var.project}-indexer"
