9dc08b1fb744aa8966ccef47988b1353765dfe74
Daithi Hearn <daithi.hearn@vechain.org>
2026-02-12 10:41:01 +0100

    Updating HistoricProposalsRepository to extend BasePagingAndSortingIndexedRepository

--- packages/common/src/main/kotlin/org/vechain/indexer/vevote/HistoricProposalsRepository.kt
+++ packages/common/src/main/kotlin/org/vechain/indexer/vevote/HistoricProposalsRepository.kt
@@ -3,16 +3,13 @@ package org.vechain.indexer.vevote
 import org.springframework.context.annotation.Profile
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Slice
-import org.springframework.data.mongodb.repository.Query
 import org.springframework.stereotype.Repository
-import org.vechain.indexer.BaseIndexedRepository
+import org.vechain.indexer.BasePagingAndSortingIndexedRepository
 
 @Profile("vevote", "vevote-historic-proposals")
 @Repository
-interface HistoricProposalsRepository : BaseIndexedRepository<HistoricProposals, String> {
-    @Query("{ '_id': { '\$ne': '__checkpoint__' } }")
-    fun findAll(pageable: Pageable): Slice<HistoricProposals>
-
+interface HistoricProposalsRepository :
+    BasePagingAndSortingIndexedRepository<HistoricProposals, String> {
     fun findByProposalId(proposalId: String, pageable: Pageable): Slice<HistoricProposals>
 
     fun findByContractAddress(contractAddress: String, pageable: Pageable): Slice<HistoricProposals>
