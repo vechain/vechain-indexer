0fb9fb94fabbc626fe1bc98d12e7434bfd75188a
Daithi Hearn <daithi.hearn@vechain.org>
2026-02-17 18:01:16 +0100

    Update MongoDB provider volume type logic for NVME clusters

--- terraform/api/mongoatlas.tf
+++ terraform/api/mongoatlas.tf
@@ -72,7 +72,7 @@ module "mongoatlas-main-net" {
     cluster_type                 = "REPLICASET"
     auto_scaling_disk_gb_enabled = try(local.env.enabled_nets.main.mongodb.auto_scaling_disk_gb_enabled, true)
     provider_name                = "AWS"
-    provider_volume_type         = try(local.env.enabled_nets.main.mongodb.provider_volume_type, "STANDARD")
+    provider_volume_type         = can(regex("NVME", local.env.enabled_nets.main.mongodb.cluster_tier)) ? null : "STANDARD"
     provider_instance_size_name  = local.env.enabled_nets.main.mongodb.cluster_tier
     mongo_db_major_version       = "8"
 
