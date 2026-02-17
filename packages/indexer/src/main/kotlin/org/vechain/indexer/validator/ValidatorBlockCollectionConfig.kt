2dde5e883ab55c065a1dfb98630a2eea1ed7a65a
Daithi Hearn <37153003+daithihearn@users.noreply.github.com>
2026-02-17 18:10:55 +0100

    Mongo performance (#1036)

--- packages/indexer/src/main/kotlin/org/vechain/indexer/validator/ValidatorBlockCollectionConfig.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/validator/ValidatorBlockCollectionConfig.kt
@@ -93,6 +93,11 @@ open class ValidatorBlockCollectionConfig(
                         .on(ValidatorBlock::validator.name, Sort.Direction.ASC)
                         .on(ValidatorBlock::status.name, Sort.Direction.ASC)
                         .on(IndexedDocument::blockNumber.name, Sort.Direction.ASC),
+                "blocksOffline_1_status_1_blockNumber_1" to
+                    Index()
+                        .on(ValidatorBlock::blocksOffline.name, Sort.Direction.ASC)
+                        .on(ValidatorBlock::status.name, Sort.Direction.ASC)
+                        .on(IndexedDocument::blockNumber.name, Sort.Direction.ASC),
             )
         )
     }
