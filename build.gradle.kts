526f39b0d49f62c2dfecb247ac1d0b7e3eef78f7
Daithi Hearn <daithi.hearn@vechain.org>
2026-02-22 01:41:18 +0100

    Rolling back to a stable verion of indexer-core 7.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -223,7 +223,7 @@ allprojects {
         implementation("io.micrometer:micrometer-registry-datadog:1.14.5")
 
         // Core indexer dependency
-        implementation("org.vechain:indexer-core:7.1.3")
+        implementation("org.vechain:indexer-core:7.0.0")
 
         // Test dependencies
         testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.5")
