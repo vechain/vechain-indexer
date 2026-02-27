515fc70e48502a7224d28c612756b2d40ad233ae
Dan Rusnac <rusnac14@gmail.com>
2026-02-27 12:29:42 +0100

    fix: use service.save(transfers)

--- packages/indexer/src/main/kotlin/org/vechain/indexer/b3tr/treasury/TreasuryTransferProcessor.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/b3tr/treasury/TreasuryTransferProcessor.kt
@@ -1,7 +1,5 @@
 package org.vechain.indexer.b3tr.treasury
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import org.vechain.indexer.BaseProcessor
@@ -31,8 +29,6 @@ open class TreasuryTransferProcessor(
 
         val transfers = service.processEvents(entry.events())
 
-        if (transfers.isNotEmpty()) {
-            withContext(Dispatchers.IO) { service.save(transfers) }
-        }
+        service.save(transfers)
     }
 }
