c24005fa854dc2a9dd898f1f109efd187a914456
Daithi Hearn <daithi.hearn@vechain.org>
2026-02-28 21:28:59 +0100

    feat: update Datadog pipeline configuration and improve test properties

--- packages/api/src/test/kotlin/org/vechain/indexer/openapi/GenerateOpenApiSpecTest.kt
+++ packages/api/src/test/kotlin/org/vechain/indexer/openapi/GenerateOpenApiSpecTest.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -13,7 +14,31 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource(locations = ["classpath:application-test.properties"])
+@ActiveProfiles(
+    "test",
+    "nfts",
+    "transactions",
+    "transfers",
+    "history",
+    "stargate",
+    "authority-nodes",
+    "vevote",
+    "vevote-results",
+    "vevote-historic-proposals",
+    "vevote-comments",
+    "b3tr",
+    "b3tr-proposal",
+    "b3tr-actions",
+    "b3tr-x-alloc",
+    "b3tr-gm-nft",
+    "b3tr-balance",
+    "explorer",
+    "block-usage",
+    "accounts",
+    "validator",
+    "contracts",
+)
+@TestPropertySource(properties = ["de.flapdoodle.mongodb.embedded.version=7.0.14"])
 @Tag("openapi")
 class GenerateOpenApiSpecTest {
 
