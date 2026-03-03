c0b17269adeb0df3899fdd2d8fcb19c89310735a
ShaunKav <shaun.kavanagh@vechain.org>
2026-03-03 18:07:40 +0900

    Fix tf plan issues

--- terraform/vpc/outputs.tf
+++ terraform/vpc/outputs.tf
@@ -28,7 +28,7 @@ output "chatbot_sns_topic_name" {
   value       = aws_sns_topic.chatbot_sns_topic.name
 }
 
-output "atlas_export_s3_bucket_id" {
+output "atlas_export_bucket_id" {
   description = "The S3 bucket ID for use in backup schedule export blocks"
   value       = local.env.environment == "prod" ? aws_s3_bucket.atlas_export_backups[0].id : ""
 }
