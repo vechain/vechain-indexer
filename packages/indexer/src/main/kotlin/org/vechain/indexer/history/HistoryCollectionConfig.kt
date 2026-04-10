10b2ddea54993a64a5aa97a1d09a922103122744
Daithi Hearn <daithi.hearn@vechain.org>
2026-04-10 10:38:23 +0200

    feat: add new indexes for event and block data in history and validator collections

--- packages/indexer/src/main/kotlin/org/vechain/indexer/history/HistoryCollectionConfig.kt
+++ packages/indexer/src/main/kotlin/org/vechain/indexer/history/HistoryCollectionConfig.kt
@@ -60,6 +60,17 @@ open class HistoryCollectionConfig(
                     Index()
                         .on(IndexedHistoryEvent::tokenId.name, Sort.Direction.ASC)
                         .on(IndexedHistoryEvent::blockTimestamp.name, Sort.Direction.DESC),
+                "tokenId_1_eventName_1_blockTimestamp_-1" to
+                    Index()
+                        .on(IndexedHistoryEvent::tokenId.name, Sort.Direction.ASC)
+                        .on(IndexedHistoryEvent::eventName.name, Sort.Direction.ASC)
+                        .on(IndexedHistoryEvent::blockTimestamp.name, Sort.Direction.DESC),
+                "contractAddress_1_tokenId_1_eventName_1_blockTimestamp_-1" to
+                    Index()
+                        .on(IndexedHistoryEvent::contractAddress.name, Sort.Direction.ASC)
+                        .on(IndexedHistoryEvent::tokenId.name, Sort.Direction.ASC)
+                        .on(IndexedHistoryEvent::eventName.name, Sort.Direction.ASC)
+                        .on(IndexedHistoryEvent::blockTimestamp.name, Sort.Direction.DESC),
                 "delegationId_1_blockNumber_-1_delegationLifecycleOrder_-1" to
                     Index()
                         .on(IndexedHistoryEvent::delegationId.name, Sort.Direction.ASC)
