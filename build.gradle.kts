fb436f391595070345cc1740d0c65a710a2732bd
Daithi Hearn <daithi.hearn@vechain.org>
2026-05-06 01:42:46 +0200

    fix: update indexer-core dependency to version 9.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -245,7 +245,7 @@ allprojects {
         implementation("io.micrometer:micrometer-registry-datadog:1.14.5")
 
         // Core indexer dependency
-        implementation("org.vechain:indexer-core:9.0.0")
+        implementation("org.vechain:indexer-core:9.0.1")
 
         // Test dependencies
         testImplementation("org.springframework.boot:spring-boot-starter-test:3.5.12")
